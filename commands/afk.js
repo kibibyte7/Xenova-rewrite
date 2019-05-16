ce334533973521c398c9181718d1b72efaf851e3
pafad <32877302+pafad@users.noreply.github.com>
2019-05-17 00:37:56 +0200

    Create afk.js

---
+++ commands/afk.js
@@ -0,0 +1,33 @@
+const Command = require("../modules/Command.js");
+class Afk extends Command {
+constructor(client) {
+super(client, {
+name:"afk",
+description:"Met un message de afk quand l'utilisateur est mentionné.",
+usage:"afk [texte]"
+})
+
+} 
+
+run(message, args, level)
+
+const afkUrl = process.env.afk;
+            request(afkUrl, (err, res, body) => {
+        
+                
+                console.log('chargement !')
+                
+                if(err || res.statusCode!== 200)return
+                
+                console.log('chargé avec succés')
+                var afk = JSON.parse(body)
+                if(!afk[message.guild.id + message.author.id]) afk[message.guild.id + message.author.id] = {};
+                if(!afk[message.guild.id + message.author.id].reason) afk[message.guild.id + message.author.id].reason = args.length == 0 ? "AFK" : `${args.join(" ")}`;
+                if(!afk[message.guild.id + message.author.id].time) afk[message.guild.id + message.author.id].time = new Date().getTime() + 120000;
+                request({ url: afkUrl, method: 'PUT', json: afk})
+
+                message.reply(`Tu es maintenant en afk pour : **${afk[message.guild.id + message.author.id].reason}**.`)
+               }) 
+} 
+
+module.exports = Afk;           
