55c60ded9cd6cd7a9cafd52bd30704f1cec40826
pafad <32877302+pafad@users.noreply.github.com>
2019-05-16 23:59:49 +0200

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,48 +1,68 @@
+/* eslint-disable */
+
 module.exports = class {
-constructor(client) {
-this.client = client;
-} 
+  constructor(client) {
+    this.client = client;
+  }
 
-async run(message) {
-	if(message.author.bot) return;
-	
-	if(!message.channel.permissionsFor(message.guild.me).missing("SEND_MESSAGES")) return;
+  async run(message) {
+    if (message.author.bot) return;
 
-  const settings = this.client.getSettings(message.guild);
-  this.settings = settings;
-  
-  if(message.content.indexOf(settings.prefix) !== 0) return;
-  
-  const args = message.content.slice(settings.prefix.length).trim().split(/ + /g) 
-  const command = args.shift().toLowerCase();
-  
-  if(message.guild && !message.member) await message.guild.fetchMember(message.author)
-  
-  const level = this.client.permLevel(message)
-  
-  const cmd = this.client.commands.get(command) || this.client.commands.get(this.client.aliases.get(command))
- 
-  if(!cmd) return;
-  
-  if(level < this.client.levelCache[cmd.conf.permLevel]){
-  	
-  	if(settings.systemNotice === "true") {
-  		
-  		return message.channel.send(`${this.client.emojis.find("name","wrongMark")} Tu n'as pas la permission d'utiliser la commande\nTon niveau de permissions: **${level} ${this.client.permLevels.find(l => l.level === level).name}**\nLe niveau de permissions requis: **${this.client.levelCache[cmd.conf.permLevel]} ${cmd.conf.permLevel}**`) 
-  		
-  		} else {
-  			
-  			return;
-  			
-  		} 
-  	
-  	}
-   
-   message.author.permLevel = level;
-  
-   cmd.run(message, args, level) 
-   this.client.logger.log(`${this.client.config.permLevels.find(l => l.level).name} ${message.author.username} a utilisé la commande ${cmd.help.name}`, "log")
-  
+    if (
+      !message.channel.permissionsFor(message.guild.me).missing("SEND_MESSAGES")
+    )
+      return;
 
- } 
-} 
+    // Paramètres
+    const settings = this.client.getSettings(message.guild);
+    message.settings = settings;
+
+    if (message.content.indexOf(settings.prefix) !== 0) return;
+
+    const args = message.content
+      .slice(settings.prefix.length)
+      .trim()
+      .split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    if (message.guild && !message.member)
+      await message.guild.fetchMember(message.author);
+
+    const level = this.client.permlevel(message);
+
+    const cmd =
+      this.client.commands.get(command) ||
+      this.client.commands.get(this.client.aliases.get(command));
+    if (!cmd) return;
+
+    if (level < this.client.levelCache[cmd.conf.permLevel]) {
+      if (settings.systemNotice === "true") {
+        return message.channel
+          .send(`Vous n'avez pas la permission pour utiliser cette commande.
+        Votre niveau de permission est ${level} (${
+          this.client.config.permLevels.find(l => l.level === level).name
+        })
+        Cette commande requirt le niveau de permission: ${
+          this.client.levelCache[cmd.conf.permLevel]
+        } (${cmd.conf.permLevel})`);
+      } else {
+        return;
+      }
+    }
+
+    message.author.permLevel = level;
+
+    message.flags = [];
+    while (args[0] && args[0][0] === "-") {
+      message.flags.push(args.shift().slice(1));
+    }
+
+    // Lancement de la commande
+    this.client.logger.log(
+      `${message.author.username} (${message.author.id} - ${
+        this.client.config.permLevels.find(l => l.level === level).name
+      }) lance la commande ${cmd.help.name}`
+    );
+    cmd.run(message, args, level);
+  }
+};
