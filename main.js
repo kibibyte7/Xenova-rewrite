71059bbc1efb178e4478f87b3f0384f7d6a929aa
pafad <32877302+pafad@users.noreply.github.com>
2019-05-16 23:34:23 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,28 +1,28 @@
 const { Client, Collection } = require("discord.js");
 const { promisify } = require("util");
 const readdir = promisify(require("fs").readdir);
-const Enmap = require("enmap")
-const klaw = require("klaw")
-const path = require("path")
+const Enmap = require("enmap");
+const klaw = require("klaw");
+const path = require("path");
 
 class Xenova extends Client {
  constructor(option){ 
- super(option) 
+ super(option); 
  
- this.config = require("./modules/Config.js")
+ this.config = require("./modules/Config.js");
  
- this.commands = new Collection() 
- this.aliases = new Collection() 
+ this.commands = new Collection(); 
+ this.aliases = new Collection(); 
  
  this.settings = new Enmap({
  	name:"settings", 
  	cloneLevel:"deep", 
  	fetchAll:false, 
  	autoFetch:true
- 	}) 
+ 	});
  	
- this.logger = require("./modules/Logger")
- this.wait = require("util").promisify(setTimeout) 
+ this.logger = require("./modules/Logger");
+ this.wait = require("util").promisify(setTimeout);
  }
  
  //Permissions 
@@ -31,13 +31,13 @@ class Xenova extends Client {
  	
  	const permOrder = this.config.permLevels
  	.slice(0)
- 	.sort((p, c) => p.level < c.level ? 1 : -1)
+ 	.sort((p, c) => p.level < c.level ? 1 : -1);
  	
  	while(permOrder.length){
- 		const currentLevel = permOrder.shift()
+ 		const currentLevel = permOrder.shift();
  		if(message.guild && currentLevel.guildOnly) continue;
  		if(currentLevel.check(message)){
- 			 permlvl = currentLevel.level
+ 			 permlvl = currentLevel.level;
  			 break;
  			}
  		} 
@@ -48,15 +48,15 @@ class Xenova extends Client {
    	try{
  	  const props = new (require(`${commandPath}${path.sep}${commandName}`))(
  	  	this
- 	  	) 
+ 	  	); 
  	  	this.logger.log(`Chargement de la commande: ${commandName}`, "log");
  	  	props.conf.location = commandPath;
  	  	if(props.init) {
- 	  	   props.init(this)
+ 	  	   props.init(this);
  	  	} 
- 	  	this.commands.set(props.help.name)
+ 	  	this.commands.set(props.help.name);
  	  	props.conf.aliases.forEach(alias => {
- 	  	this.aliases.set(alias, props.help.name)
+ 	  	this.aliases.set(alias, props.help.name);
  	  }) 
  	  return false;
  	  } catch (e) {
@@ -65,18 +65,18 @@ class Xenova extends Client {
  	} 
  	
  	getSettings(guild){
- 	const defaults = this.config.defaultSettings || {} 
- 	const guildData = this.settings.get(guild.id) || {} 
+ 	const defaults = this.config.defaultSettings || {}; 
+ 	const guildData = this.settings.get(guild.id) || {};
  	const returnObject = {} 
  	Object.keys(defaults).forEach(key => {
- 		returnObject[key] = guildData[key] ? guildData[key] : defaults[key] 
+ 		returnObject[key] = guildData[key] ? guildData[key] : defaults[key]; 
  		}) 
  		return returnObject;
  	} 
 } 
 
-const client = new Xenova()
-console.log(client.config.permLevels.map(p => `${p.level} : ${p.name}`))
+const client = new Xenova();
+console.log(client.config.permLevels.map(p => `${p.level} : ${p.name}`));
 
 //Fonction d'initialisation
 
@@ -85,29 +85,29 @@ const init = async () => {
 klaw("./commands").on('data', item => {
 	const cmdFile = path.parse(item.path);
 	if(!cmdFile.ext || cmdFile.ext !== ".js") return;
-	const response = client.loadCommands(cmdFile.dir, `${cmdFile.name}${cmdFile.ext}`)
-	if(response) client.logger.error(response)
+	const response = client.loadCommands(cmdFile.dir, `${cmdFile.name}${cmdFile.ext}`);
+	if(response) client.logger.error(response);
 	}) 
 	
 	//recup des événements 
-	const evtFiles = await readdir("./events")
-	client.logger.log(`Chargement de ${evtFiles.length} événements.`, "log") 
+	const evtFiles = await readdir("./events");
+	client.logger.log(`Chargement de ${evtFiles.length} événements.`, "log");
 	evtFiles.forEach(file => {
-		const evtName = file.split(".")[0] 
-		client.logger.log(`Chargement de l'événement: ${evtName}`,"log")
-		const event = new (require(`./events/${file}`))(client) 
-		client.on(evtName, (...args) => event.run(...args))
-		delete require.cache[require.resolve(`./events/${file}`)] 
+		const evtName = file.split(".")[0];
+		client.logger.log(`Chargement de l'événement: ${evtName}`,"log");
+		const event = new (require(`./events/${file}`))(client); 
+		client.on(evtName, (...args) => event.run(...args));
+		delete require.cache[require.resolve(`./events/${file}`)];
 		}) 
 		
 		client.levelCache = {} 
 		
 		for(let i = 0; i < client.config.permLevels.length; i++) {
-			const thisLevel = client.config.permLevels[i]
+			const thisLevel = client.config.permLevels[i];
 			client.levelCache[thisLevel.name] = thisLevel.level;
 		} 
 			
-		client.login(process.env.token) 
+		client.login(process.env.token); 
 } 
 
 init();
