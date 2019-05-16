b2f139b32b1700fcabfbb9e0b41ba6777ed8336a
pafad <32877302+pafad@users.noreply.github.com>
2019-05-16 16:58:38 +0200

    Create ready.js

---
+++ events/ready.js
@@ -0,0 +1,17 @@
+module.exports = class {
+constructor(client) 
+this.client = client
+async run() {
+	await this.client.wait(1000)
+	
+	this.client.appInfo = this.client.fetchApplication();
+	setInterval(async () => {
+	this.client.fetchApplication()
+	}, 60000)
+	
+	this.client.user.setActivity(`${this.client.config.prefix}help, {type:"STREAMING"})
+	
+	this.client.logger.log(`${this.client.user.tag} lancé avec succès.`, "ready") 
+	
+	} 
+} 
