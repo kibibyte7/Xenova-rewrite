45c3824b6f35aacf40f11dee60c84b7f19d5455a
pafad <32877302+pafad@users.noreply.github.com>
2019-05-16 18:25:02 +0200

    Update Command.js

--- modules/Command.js
+++ modules/Command.js
@@ -1,14 +1,14 @@
 class Command {
  constructor(
  	client, {
- 	name: "Aucun nom défini.", 
- 	description:"Aucune description définie.", 
- 	category:"Aucune catégorie définie.", 
- 	usage:"Aucune utilisation définie.", 
- 	enable:true, 
- 	guildOnly:false, 
- 	aliases:Array(), 
- 	permLevel:"Utilisateur" 
+ 	name = "Aucun nom défini.", 
+ 	description = "Aucune description définie.", 
+ 	category = "Aucune catégorie définie.", 
+ 	usage = "Aucune utilisation définie.", 
+ 	enable = true, 
+ 	guildOnly = false, 
+ 	aliases = Array(), 
+ 	permLevel = "Utilisateur" 
  	}
  	) {
  	this.client = client;
