dca0d0730f4a15bad286d56141ed1b89d965be2e
pafad <32877302+pafad@users.noreply.github.com>
2019-05-17 12:58:35 +0200

    Create eval.js

---
+++ commands/eval.js
@@ -0,0 +1,34 @@
+const Command = require("../modules/Command.js") 
+
+class Eval extends Command {
+constructor(client) {
+super(client, {
+name:"eval", 
+description:"Teste un code donné par le Owner.", 
+usage:"eval <code>", 
+permLevel:"XenoOwner
+}) 
+} 
+
+run(message, args, level) {
+                try {
+      const code = args.join(" ");
+      let evaled = eval(code);
+
+      if (typeof evaled !== "string")
+        evaled = require("util").inspect(evaled);
+      message.channel.send('\`\`résultat\`\`'+ `\`\`\`xl\n${clean(evaled)}\n\`\`\``) 
+    } catch (err) {
+      message.channel.send("\`\`erreur\`\`" +  `\`\`\`xl\n${clean(err)}\n\`\`\``)
+    }
+  
+  function clean(text) {
+  if (typeof(text) === "string")
+    return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+  else
+      return text;
+  }
+ 
+}
+
+module.exports = Eval;
