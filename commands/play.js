9a650d510a24da8b0790f471f126a574e9aefae4
pafad <32877302+pafad@users.noreply.github.com>
2019-05-17 17:59:06 +0200

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -1,79 +1,82 @@
-const Command = require("../modules/Command.js") 
-
-const ytdl = require("ytdl-core")
-const ydtldiscord = require ("ytdl-core-discord") 
-const { Util } = require("discord.js") 
+const Command = require("../../modules/Command.js");
+const ytdl = require("ytdl-core");
+const ytdlDiscord = require("ytdl-core-discord");
+const { Util } = require("discord.js");
 
 class Play extends Command {
-	constructor(client) {
-	super(client, {
-	name:"play",
-	description:"Joue la musique souhaitée en vocal.",
-	usage:"play <lien ou titre>", 
-	aliases:["p"] 
-	}) 
-	} 
-	
-	
-async run(message, args, level){
-const { voiceChannel } = message.member;
-if(!voiceChannel) return message.channel.send(`${this.client.emojis.find("name", "wrongMark")} Tu n'es pas dans un channel vocal.`);
-  const serverQueue = this.client.queue.get(message.guild.id) 
-  const songInfo = ytdl.getInfo(args[0]);
-  const song = {
-  title:"non défini", 
-  url:args[0],
-  requester:message.author.username
-  } 
-  
-  if(serverQueue) {
-  serverQueue.songs.push(song)
-  return message.channel.send(`${this.client.emojis.find("name", "checkMark")} **${song.title}** a été ajouté à la playlist.`)
-  } 
-  
-  const queueConstruct = {
-  textChannel:message.channel,
-  voiceChannel,
-  connection:null,
-  songs:[], 
-  playing:true, 
-  voulume:1
-  } 
-  
-  this.client.queue.set(message.guild.id, queueConstruct) 
-  queueConstruct.songs.push(song) 
-  const play = async song => {
-  	const queue = this.client.queue.get(message.guil.id)
-  	if(!queue){
-  	 queue.voiceChannel.leave()
-  	 this.client.queue.delete(message.guild.id)
-  	 return;
-  		}
-  		
-  		const dispatcher = queue.connection
-  		.playOpusStream(await ytdldiscord(song.url), {passes: 3})
-  		.on('end', reason => {
-  			if(reason === "Timeout")
-  			console.log("La musique s'est arrêté.") 
-  			else console.log(reason)
-  			queue.songs.shift()
-  			play(queue.songs[0])
-  			}).on("error", e => console.log(e)) 
-  			dispatcher.setVolumeLogarithmic(queue.volume / 5)
-  			queue.textChannel.send(`Je joue **${song.title}** demandé par **${song.requester}**.`)
-  	} 
-  	try{
-  	const connection = voiceChannel.join();
-  	queueConstruct.connection = connection;
-  	play(queueConstruct.songs[0])
-  	} catch (e) {
-  	message.channel.send(`${this.client.emojis.find("name", "wrongMark")} Je n'ai pas pu rejoindre le channel vocal: **${e}**`)
-  	this.client.queue.delete(message.guild.id)
-  	await voiceChannel.leave()
-  	} 
-  
+  constructor(client) {
+    super(client, {
+      name: "play",
+      description: "Jouer et ajouter de la musique.",
+      usage: "play"
+    });
   }
-} 
 
+  async run(message, args) {
+    const { voiceChannel } = message.member;
+    if (!voiceChannel)
+      return message.channel.send(
+        "Vous devez être dans un salon vocal pour utiliser cette commande !"
+      );
+
+    const serverQueue = message.client.queue.get(message.guild.id);
+    const songInfo = await ytdl.getInfo(args[0]);
+    const song = {
+      id: songInfo.video_id,
+      title: Util.escapeMarkdown(songInfo.title),
+      url: songInfo.video_url
+    };
+
+    if (serverQueue) {
+      serverQueue.songs.push(song);
+      return message.channel.send(
+        `✅ **${song.title}** est ajoutée à la queue !`
+      );
+    }
+
+    const queueConstruct = {
+      textChannel: message.channel,
+      voiceChannel,
+      connection: null,
+      songs: [],
+      volume: 1,
+      playing: true
+    };
+    message.client.queue.set(message.guild.id, queueConstruct);
+    queueConstruct.songs.push(song);
+
+    const play = async song => {
+      const queue = message.client.queue.get(message.guild.id);
+      if (!song) {
+        queue.voiceChannel.leave();
+        message.client.queue.delete(message.guild.id);
+        return;
+      }
+
+      const dispatcher = queue.connection
+        .playOpusStream(await ytdlDiscord(song.url), { passes: 3 })
+        .on("end", reason => {
+          if (reason === "Récupération trop lente !")
+            console.log("La musique s'est arrêtée !");
+          else console.log(reason);
+          queue.songs.shift();
+          play(queue.songs[0]);
+        })
+        .on("error", error => console.error(error));
+      dispatcher.setVolumeLogarithmic(queue.volume / 5);
+      queue.textChannel.send(`🎶 Commence à jouer: **${song.title}**`);
+    };
+
+    try {
+      const connection = await voiceChannel.join();
+      queueConstruct.connection = connection;
+      play(queueConstruct.songs[0]);
+    } catch (error) {
+      console.error(`Je n'ai pas pu rejoindre le salon: ${error}`);
+      message.client.queue.delete(message.guild.id);
+      await voiceChannel.leave();
+    }
+  }
+}
 
 module.exports = Play;
