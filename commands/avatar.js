9359c4af16a36aa3267a5edb1dd2882ea0c055e9
pafad <32877302+pafad@users.noreply.github.com>
2019-05-17 15:46:58 +0200

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,69 +1,78 @@
 const Command = require("../modules/Command.js") 
 const sm = require("string-similarity")
 class Avatar extends Command {
-constructor(client) {
-super(client, {
-name:"avatar", 
-description:"Montre l'avatar", 
-usage:"avatar [@mention]", 
-aliases:["pp"] 
-}) 
-} 
-} 
+	constructor(client) {
+	super(client, {
+	name:"avatar",
+	description:"Montre l'avatar",
+	usage:"avatar [@mention]", 
+	aliases:["pp"] 
+	}) 
+	} 
+	
+	
+	run(message, args, level){
+		
+		let membres = []; 
+		
+		let indexes = []; 
+		
+		message.guild.members.forEach(function(member){ 
+			membres.push(member.user.username) 
+			indexes.push(member.id) 
+			}) 
+			
+			let match = sm.findBestMatch(args.join(" "), membres); 
+			
+			let username = match.bestMatch.target; 
+			
+			let ToShow = message.guild.members.get(indexes[membres.indexOf(username)]); 
+			
+			let user_avatar = message.mentions.members.first() || message.guild.members.find("id", args.join(" ")) || ToShow; 
+			
+			if(!args || args.length < 1){ 
+				
+			message.channel.send({embed:{ 
+			color: Math.floor(Math.random() * 16777214) + 1, 
+			author: { 
+			name: message.author.tag,
+			icon_url: message.author.avatarURL
+			}, 
+			title: `avatar de ${message.author.tag}`, 
+			url: message.author.avatarURL, 
+			image: { 
+		 url: message.author.avatarURL 
+		 }, 
+		 timestamp: new Date(), 
+		 footer: { 
+		 icon_url: client.user.avatarURL, 
+		 text: `avatar`
+		 }
+		 }}) 
+		 
+		 }else{ 
+		 	
+		 message.channel.send({embed:{ 
+		 color: Math.floor(Math.random() * 16777214) + 1, 
+		 author: {
+		 name: message.author.tag,
+		 icon_url: message.author.avatarURL
+		 },
+		 title: `Avatar de ${user_avatar.user.tag}`,
+		 url: user_avatar.user.avatarURL,
+		 image: { 
+		 url: user_avatar.user.avatarURL 
+		 }, 
+		 timestamp: new Date(),
+		 footer: {
+		 icon_url: client.user.avatarURL,text: `avatar`
+		 }
+		 }})
+		 } 
+		 } 
+		 }
 
-run(message, args, level){
-let membres = [];
-    let indexes = [];
-    message.guild.members.forEach(function(member){
-        membres.push(member.user.username)
-        indexes.push(member.id)
-    })
 
-    let match = sm.findBestMatch(args.join(" "), membres);
-    let username = match.bestMatch.target;
 
-    let ToShow = message.guild.members.get(indexes[membres.indexOf(username)]);
-
-    let user_avatar = message.mentions.members.first() || message.guild.members.find("id", args.join(" ")) || ToShow;
-    if(!args || args.length < 1){
-        message.channel.send({embed:{
-        color: Math.floor(Math.random() * 16777214) + 1,
-        author: {
-            name: message.author.tag,
-            icon_url: message.author.avatarURL,
-        },
-        title: `avatar de ${message.author.tag}`,
-        url: message.author.avatarURL,
-        image: {
-            url: message.author.avatarURL
-        },
-        timestamp: new Date(),
-        footer: {
-            icon_url: client.user.avatarURL,
-            text: `avatar`,
-        },
-    }})     
-    }else{
-         message.channel.send({embed:{
-    color: Math.floor(Math.random() * 16777214) + 1,
-     author: {
-name: message.author.tag,
-icon_url: message.author.avatarURL,
-},
-title: `avatar de ${user_avatar.user.tag}`,
-url: user_avatar.user.avatarURL,
-image: {
-    url: user_avatar.user.avatarURL
-    },
-    timestamp: new Date(),
-footer: {
-icon_url: client.user.avatarURL,
-text: `avatar`,
-},
-}})
-}
-
-} 
-} 
 
 module.exports = Avatar;
