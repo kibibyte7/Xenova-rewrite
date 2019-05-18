1701800d0215f6703b246e0ed78f5a33b52b29a3
pafad <32877302+pafad@users.noreply.github.com>
2019-05-18 14:29:56 +0200

    Update message.js

--- events/message.js
+++ events/message.js
@@ -50,7 +50,7 @@ module.exports = class {
     }
    
     if(cmd.conf.enabled === false){
-    if(systemNotice == true) {
+    if(sttings.systemNotice == true) {
         return message.channel
          .send(`${this.client.emojis.find("name", "wrongMark")} Cette commande est désactivée suite à des bugs.`)
        } 
