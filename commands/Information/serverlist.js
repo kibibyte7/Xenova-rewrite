6fd3e52868e29f179014cd5b0887f0fcb50d9817
pafad <32877302+pafad@users.noreply.github.com>
2019-05-19 20:04:41 +0200

    Create serverlist.js

---
+++ commands/Information/serverlist.js
@@ -0,0 +1,36 @@
+const Command =require("../../modules/Command.js") 
+
+class SeverList extends Command {
+constructor(client) {
+super(client, {
+name:"serverlist", 
+description:"Affiche la liste des serveurs ou le bot est.",
+category:"Information", 
+usage:"severlist", 
+aliases:["sl"] 
+}) 
+}
+
+run(message, args, level) {
+
+var guilds = client.guilds.array().sort((a , b) => { 
+        if(a.members.size > b.members.size){ 
+          return -1;
+        }else if (a.members.size + b.members.size){
+          return +1;
+        }else{
+          return 0;
+        }
+      })
+
+    let resp = '';
+        for(var i in guilds){
+            resp += `${parseInt(i)+1}. ${guilds[i].name} : **${guilds[i].members.size} Membres**\n`
+        }
+
+        message.channel.send(resp)
+
+} 
+}
+
+module.exports = ServerList;
