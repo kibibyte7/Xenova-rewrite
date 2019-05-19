5c90d75dd5ba71d404c04f574e79e03323576aa3
pafad <32877302+pafad@users.noreply.github.com>
2019-05-19 12:42:24 +0200

    Update ball.js

--- commands/Fun/ball.js
+++ commands/Fun/ball.js
@@ -13,6 +13,8 @@ aliases:["b"]
 } 
 
 run(message, args, level) {
+if(!args[0]) return message.channel.send(`${this.client.emojis.find("name", "wrongMark")} Pose une question.\n${this.client.config.defaulrSettings.prefix}ball est-ce un test ?`)
+ 
 const response = ["oui.","non.","peut-être que oui peut-être que non.","absoluement !","absoluement pas.","essaye encore.","concentre toi bien et demande ça plus tard.","chut c'est secret.","je ne peux pas le prédire.","je sais pas.","j'ai des sources qui disent que oui.","j'ai des sources qui disent que non.","j'ai pas d'info à donner.","redemande plus tard.","je pourrai le dire, sauf que j'ai pas envie.","définitivement oui.","pas du tout.","à coté de la plaque."]
 let r = rn({
                min: 0,
@@ -20,8 +22,7 @@ let r = rn({
                integer: true
            });
        let ball = response[r];
-       message.channel.send(`:gem: ${message.author}` + ` ${ball}`)
-       }
+       message.channel.send(`:gem: ${message.author}` + ` ${ball}`)      
 } 
 }
 
