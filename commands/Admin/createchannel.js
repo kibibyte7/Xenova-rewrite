7a5ed61625c3bdf6bb00ee8006cbedd8ebf53f55
pafad <32877302+pafad@users.noreply.github.com>
2019-05-19 11:37:34 +0200

    Create createchannel.js

---
+++ commands/Admin/createchannel.js
@@ -0,0 +1,33 @@
+const Command = require("../../modules/Command.js")
+
+class CreateChannel extends Command {
+constructor(client) {
+super(client, {
+name:"createchannel", 
+description:"Le bot crée un channel textuel.", 
+category:"Admin", 
+usage:"createchannel <nom du channel>",
+permLevel:"XenoAdminPerm", 
+aliases:["addchan"] 
+}) 
+} 
+
+run(message, args, level) {
+if(!message.member(client.user).hasPermission("MANAGE_CHANNELS")) {
+      message.channel.send(`${this.client.emojis.find("name", "wrongMark")} Je n'ai pas la permission de gérer les salons.`) 
+      return;
+      } else { 
+if(!args || args.length < 1){
+         message.channel.send(`${this.client.emojis.find("name", "wrongMark")} Spécifie le nom de la channel textuel à créer.`)
+         return;     
+      } else {
+       message.guild.createChannel(args.join(" "), "text")
+       message.channel.send(`${this.client.emojis.find("name", "checkMark")} Catégorie: **${args.join(" ")}** créée avec succès !`)
+        }
+      }
+} 
+
+} 
+}
+
+module.exports = CreateChannel;
