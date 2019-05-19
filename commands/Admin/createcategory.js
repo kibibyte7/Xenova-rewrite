5ecd1027987e6d0c6d8f1f3a20fd3600eca05c63
pafad <32877302+pafad@users.noreply.github.com>
2019-05-19 11:30:59 +0200

    Create createcategory.js

---
+++ commands/Admin/createcategory.js
@@ -0,0 +1,33 @@
+const Command = require(../../modules/Command.js")
+
+class CreateCategory extends Command {
+constructor(client) {
+super(client, {
+name:"createcategory", 
+description:"Le bot crée une catégorie.", 
+category:"Admin", 
+usage:"createcategory <nom du channel>",
+permLevel:"XenoAdminPerm", 
+aliases:["addcat"] 
+}) 
+} 
+
+run(message, args, level) {
+if(!args || args.length < 1){
+         message.channel.send(`${this.client.emojis.find("name", "wrongMark")} Spécifie le nom de la catégorie à créer.`)
+         return;
+    }else{
+      if(!message.member(client.user).hasPermission("MANAGE_CHANNELS")) {
+      message.channel.send(`${this.client.emojis.find("name", "wrongMark")} Je n'ai pas la permission de gérer les salons.`) 
+      return;
+      } else {
+       message.guild.createChannel(args.join(" "), "category")
+       message.channel.send(`${this.client.emojis.find("name", "checkMark")} Catégorie: **${args.join(" ")}** créée avec succès !`)
+        }
+      }
+} 
+
+} 
+}
+
+module.exports = CreateCategory
