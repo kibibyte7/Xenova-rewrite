240bd74e2639e8882c489c0e668b4148c08a530e
pafad <32877302+pafad@users.noreply.github.com>
2019-05-30 16:17:39 +0200

    Update hook.js

--- commands/Fun/hook.js
+++ commands/Fun/hook.js
@@ -11,10 +11,10 @@ category:"Fun"
 
 run(client, message, args) {
 
-message.delete() 
+message.delete();
  		
  		if(!args || args.length == 0){
- 			return tools.hook(message.channel, `Help`, `${prefix}hook <message>`, "00ffff", "https://cdn.discordapp.com/attachments/564478330712096770/571327152818225171/d4963add60654027bad8e894a3779ae1.jpg")
+ 			return WH.hook(message.channel, `Help`, `${this.client.config.defaultSettings.prefix}hook <message>`, "00ffff", "https://cdn.discordapp.com/attachments/564478330712096770/571327152818225171/d4963add60654027bad8e894a3779ae1.jpg")
  			}
  			
  			let role = message.member.roles.last();
