e9b665f00b8d8565fc1981c72c80bc06993b693e
pafad <32877302+pafad@users.noreply.github.com>
2019-05-30 16:34:05 +0200

    Update hook.js

--- commands/Fun/hook.js
+++ commands/Fun/hook.js
@@ -15,12 +15,12 @@ run(message, args, level) {
 message.delete();
  		
  		if(!args || args.length == 0){
- 			return WH.hook(message.channel, `Help`, `${this.client.config.defaultSettings.prefix}hook <message>`, "00ffff", "https://cdn.discordapp.com/attachments/564478330712096770/571327152818225171/d4963add60654027bad8e894a3779ae1.jpg")
+ 			return WH(message.channel, `Help`, `${this.client.config.defaultSettings.prefix}hook <message>`, "00ffff", "https://cdn.discordapp.com/attachments/564478330712096770/571327152818225171/d4963add60654027bad8e894a3779ae1.jpg")
  			}
  			
  			let role = message.member.roles.last();
 
- 			WH.hook(message.channel, message.author.username, args.join(" "), role.hexColor.slice(1), message.author.avatarURL) 
+ 			WH(message.channel, message.author.username, args.join(" "), role.hexColor.slice(1), message.author.avatarURL) 
 
 function WH(channel, title, message, color, avatar) {
 if(!channel) return console.log("Pas de channel spécifié.")
