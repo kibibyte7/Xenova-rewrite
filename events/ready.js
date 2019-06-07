a44258c203715eb74839097b4c1e058a02f7c3c8
pafad <32877302+pafad@users.noreply.github.com>
2019-06-07 10:55:50 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -11,6 +11,17 @@ async run() {
 	this.client.fetchApplication()
 	}, 60000)
 	
+        this.client.channels.get("564478327914627082").send({embed:{
+		color:0x010101, 
+		author:{
+                name:"Lancement réussi"
+                },
+		description:"Bot démarré avec succès !",
+		timestamp:new Date(),
+		footer:{
+                icon_url:client.user.avatarURL,text:"lancé"
+		} 
+
 	this.client.user.setActivity(`${this.client.config.defaultSettings.prefix}help | ${this.client.guilds.size} servs | ${this.client.users.size} utilisateurs`, {type:"STREAMING"})
 	
 	this.client.logger.log(`${this.client.user.tag} lancé avec succès.`, "ready") 
