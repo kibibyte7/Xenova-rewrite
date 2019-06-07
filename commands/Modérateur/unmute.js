d550085c78bc026ff3bc0817d7aba85774f5cd50
pafad <32877302+pafad@users.noreply.github.com>
2019-06-07 21:24:18 +0200

    Update unmute.js

--- commands/Modérateur/unmute.js
+++ commands/Modérateur/unmute.js
@@ -22,7 +22,9 @@ const wrong = this.client.emojis.find("name", "wrongMark")
 if(!mention) return message.channel.send(`${wrong} Mentionne quelqu'un.`) 
 
 if(!message.guild.me.hasPermission("MANAGE_CHANNELS")) return message.channel.send(`${wrong} Je n'ai pas la permission de gérer les channels.`)
- 
+
+if(mention.hasPermission("SEND_MESSAGES")) return message.channel.send(`${wrong} Cet utilisateur n'est pas mute.`) 
+
 message.channel.send(`${this.client.emojis.find("name", "typing")} Veux tu vraiment unmute **${mention.user.username}** ?`).then(m => {
 
 m.react(check)
@@ -39,7 +41,11 @@ if(r.emoji.name == check.name){
 	     		
 r.remove(message.author);
 
-message.guild.channels.map(c => c.permissionOverwrites.get(mention.user.id).delete())        
+message.guild.channels.map(c => c.overwritePermissions(mention.user.id, {
+"SEND_MESSAGES":true, 
+"CONNECT":true, 
+"ADD_REACTIONS":true, 
+})) 
 
 m.clearReactions()
 
