2d0fbbe07ffad4e90e49a841155b82e09bf68028
pafad <32877302+pafad@users.noreply.github.com>
2019-06-15 00:19:37 +0200

    Create guildDelete.js

---
+++ events/guildDelete.js
@@ -0,0 +1,11 @@
+module.exports = class {
+  constructor (client) {
+    this.client = client;
+  }
+
+  async run (guild) {
+
+    this.client.user.setActivity(`${this.client.config.defaultSttings.prefix}help | ${this.client.guilds.size} servs | ${this.client.users.size} utilisateurs`, {type:"STREAMING"});
+    this.client.channels.get("").send(`[-] Le bot a quitté une guilde : **${guild.name} (${guild.id})** avec **${guild.memberCount - 1} membres**`);
+  }
+};
