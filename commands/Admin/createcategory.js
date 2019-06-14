e5ca2a06b722f888251a56601c5f00f0034bb438
pafad <32877302+pafad@users.noreply.github.com>
2019-06-14 08:29:53 +0200

    Update createcategory.js

--- commands/Admin/createcategory.js
+++ commands/Admin/createcategory.js
@@ -17,7 +17,7 @@ if(!args || args.length < 1){
          message.channel.send(`${this.client.emojis.find("name", "wrongMark")} Spécifie le nom de la catégorie à créer.`)
          return;
     }else{
-      if(!message.member(this.client.user).hasPermission("MANAGE_CHANNELS")) {
+      if(!message.guild.member(this.client.user).hasPermission("MANAGE_CHANNELS")) {
       message.channel.send(`${this.client.emojis.find("name", "wrongMark")} Je n'ai pas la permission de gérer les salons.`) 
       return;
       } else {
