37a61aac3d84dd7b44b6cfbe9600c9a05f946076
pafad <32877302+pafad@users.noreply.github.com>
2019-06-15 23:02:30 +0200

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -5,6 +5,6 @@ module.exports = class {
 
   async run (member) {
 
-    this.client.user.setActivity(`${this.client.config.defaultSttings.prefix}help | ${this.client.guilds.size} servs | ${this.client.users.size} utilisateurs`, {type:"STREAMING"});
+    this.client.user.setActivity(`${this.client.config.defaultSettings.prefix}help | ${this.client.guilds.size} servs | ${this.client.users.size} utilisateurs`, {type:"STREAMING"});
   }
 };
