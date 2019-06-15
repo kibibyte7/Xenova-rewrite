f4be009ee9433179e01ef40946f162d7ac43ba3e
pafad <32877302+pafad@users.noreply.github.com>
2019-06-16 01:15:43 +0200

    Update play.js

--- commands/Musique/play.js
+++ commands/Musique/play.js
@@ -1,7 +1,6 @@
 const Command = require("../../modules/Command.js");
-const ytdl = require("ytdl-core");
-const ytdlDiscord = require("ytdl-core-discord");
-const { Util } = require("discord.js");
+const Ksoft = require("ksoft.js");
+const ksoft = new Ksoft(process.env.ksoft);
 
 class Play extends Command {
   constructor(client) {
@@ -18,67 +17,47 @@ class Play extends Command {
     const { voiceChannel } = message.member;
     if (!voiceChannel)
       return message.channel.send(
-        "Vous devez être dans un salon vocal pour utiliser cette commande !"
+        "Tu dois être dans un salon vocal pour utiliser cette commande !"
       );
 
     const serverQueue = message.client.queue.get(message.guild.id);
-    const songInfo = await ytdl.getInfo(args[0]);
-    const song = {
-      id: songInfo.video_id,
-      title: songInfo.title,
-      url: songInfo.video_url
-    };
-    console.log(song) 
+    //const song = {
+      //,
+      //title: songInfo.title,
+      //url: songInfo.video_url
+   // };
+   // console.log(song) 
 
-    if (serverQueue) {
-      serverQueue.songs.push(song);
-      return message.channel.send(
+    //if (serverQueue) {
+      //serverQueue.songs.push(song);
+      //return message.channel.send(
         `✅ **${song.title}** est ajoutée à la queue !`
-      );
-    }
+      //);
+    //}
 
-    const queueConstruct = {
-      textChannel: message.channel,
-      voiceChannel,
-      connection: null,
-      songs: [],
-      volume: 1,
-      playing: true
-    };
-    message.client.queue.set(message.guild.id, queueConstruct);
-    queueConstruct.songs.push(song);
+    //const queueConstruct = {
+      //textChannel: message.channel,
+      //voiceChannel,
+      //connection: null,
+      /songs: [],
+      //volume: 1,
+      //playing: true
+    //};
+    //message.client.queue.set(message.guild.id, queueConstruct);
+    //queueConstruct.songs.push(song);
 
     const play = async song => {
-      const queue = message.client.queue.get(message.guild.id);
-      if (!song) {
-        queue.voiceChannel.leave();
-        message.client.queue.delete(message.guild.id);
-        return;
-      }
-
-      const dispatcher = queue.connection
-        .playOpusStream(await ytdlDiscord(song.url), { passes: 3 })
-        .on("end", reason => {
-          if (reason === "Récupération trop lente !")
-            console.log("La musique s'est arrêtée !");
-          else console.log(reason);
-          queue.songs.shift();
-          play(queue.songs[0]);
-        })
-        .on("error", error => console.error(error));
-      dispatcher.setVolumeLogarithmic(queue.volume / 5);
-      queue.textChannel.send(`🎶 Commence à jouer: **${song.title}**`);
-    };
+      //const queue = message.client.queue.get(message.guild.id);
+      //if (!song) {
+        //queue.voiceChannel.leave();
+        //message.client.queue.delete(message.guild.id);
+        //return;
+      //}
 
     try {
-      const connection = await voiceChannel.join();
-      queueConstruct.connection = connection;
-      play(queueConstruct.songs[0]);
-    } catch (error) {
-      console.error(`Je n'ai pas pu rejoindre le salon: ${error}`);
-      message.client.queue.delete(message.guild.id);
-      await voiceChannel.leave();
-    }
+    var connection = message.member.voiceChannel.join();
+    ksoft.lyrics.searchAndPlay(args.join(" "), connection) 
+    
   }
 }
 
