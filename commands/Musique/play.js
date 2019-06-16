fb76c3f941070b3a9e0ea4f9394505bf1334b3c4
pafad <32877302+pafad@users.noreply.github.com>
2019-06-16 07:34:37 +0200

    Update play.js

--- commands/Musique/play.js
+++ commands/Musique/play.js
@@ -57,7 +57,7 @@ class Play extends Command {
         const voiceChannel = message.member.voiceChannel;
         const connection = await voiceChannel.join();
         ksoft.lyrics.searchAndPlay(query, connection).then(res => {
-        console.log(res.json())
+        console.log(res.json)
             const embed = new Discord.MessageEmbed()
                 .setTitle('Song Info')
                 .setColor('ce0202')
