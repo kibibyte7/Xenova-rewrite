5f270f01c5c9855fa49ee09e8b3a4d7dfbb2356b
pafad <32877302+pafad@users.noreply.github.com>
2019-06-18 22:13:30 +0200

    Update shutdown.js

--- commands/Système/shutdown.js
+++ commands/Système/shutdown.js
@@ -14,6 +14,19 @@ class Shutdown extends Command {
 
   async run (message, args, level) { // eslint-disable-line no-unused-vars
     try {
+      
+        this.client.channels.get("586596535165386759").send({embed:{
+		color:0x010101, 
+		author:{
+                name:"Shutdown"
+                },
+		description:"Le bot va redémarrer dans quelques instants suite à des mises à jours.",
+		timestamp:new Date(),
+		footer:{
+                icon_url:this.client.user.avatarURL,text:"© Shutdown | Xenova"
+		} 
+                }}) 
+
       await message.reply("Extinction du bot.");
       this.client.commands.forEach(async cmd => {
         await this.client.unloadCommand(cmd);
