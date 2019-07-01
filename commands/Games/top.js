b7085f4c698c530bce9ad6fd2d3535e1c329ea31
pafad <32877302+pafad@users.noreply.github.com>
2019-07-01 23:30:07 +0200

    Create top.js

---
+++ commands/Games/top.js
@@ -0,0 +1,48 @@
+const Command = require("../../modules/Command.js")
+const mysql = require("mysql") 
+
+class Top extends Command {
+constructor(client){
+super(client, {
+name:"top", 
+description:"Classement par niveau.", 
+category:"Game", 
+usage:"top", 
+aliases:[] 
+}) 
+} 
+
+run(message, args, level) {
+
+var con = mysql.createConnection({
+host:process.env.host, 
+user:process.env.user, 
+password:process.env.password, 
+database:process.env.database 
+}) 
+
+con.connect(err => {
+if(err) throw err;
+console.log("Base de données connecté.") 
+}) 
+
+con.query("SELECT * FROM inventory ORDER BY cast (niveau as SIGNED) DESC LIMIT 25", (err, rows) => {
+ 
+let resp = ``;
+ 		
+for(var i in rows){
+	
+resp += `[${parseInt(i)+1}] - **${bot.users.get(rows[i].id).tag}:** Niveau - **${rows[i].xp}**\n`
+		
+} 
+	
+message.channel.send(resp) 	
+	
+}) 
+
+setTimeout(() => {con.end()}, 1000*60*1) 
+
+}
+}
+
+module.exports = Top;
