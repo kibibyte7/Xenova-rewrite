520a5167c9457ce906dff38f38a0a4b3a6e23ca8
pafad <32877302+pafad@users.noreply.github.com>
2019-07-01 23:56:21 +0200

    Update top.js

--- commands/Games/top.js
+++ commands/Games/top.js
@@ -26,13 +26,15 @@ if(err) throw err;
 console.log("Base de données connecté.") 
 }) 
 
-con.query("SELECT * FROM inventory ORDER BY cast (niveau as SIGNED) DESC LIMIT 25", (err, rows) => {
+con.query("SELECT * FROM inventory ORDER BY cast (niveau as SIGNED) DESC LIMIT 10", (err, rows) => {
  
 let resp = ``;
  		
 for(var i in rows){
 	
 resp += `[${parseInt(i)+1}] - **${this.client.users.find("id", `${rows[i].id}`).tag} - **Niveau: **${rows[i].niveau}**\n`
+
+if(!this.client.users.find("id", rows[i].id)) return resp += `null\n;
 		
 } 
 	
