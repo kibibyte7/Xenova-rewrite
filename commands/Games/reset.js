e874343390ea74a880a11c1d7066eb4a6628a786
pafad <32877302+pafad@users.noreply.github.com>
2019-07-02 15:37:39 +0200

    Update reset.js

--- commands/Games/reset.js
+++ commands/Games/reset.js
@@ -1,5 +1,4 @@
 const Command = require("../../modules/Command.js")
-const mysql = require("mysql") 
 
 class Reset extends Command {
 constructor(client){
@@ -13,19 +12,7 @@ aliases:[]
 }) 
 } 
 
-run(message, args, level) {
-
-var con = mysql.createConnection({
-host:process.env.host, 
-user:process.env.user, 
-password:process.env.password, 
-database:process.env.database 
-}) 
-
-con.connect(err => {
-if(err) throw err;
-console.log("Base de données connecté.") 
-})
+run(message, args, level, con) {
 
 var mention = message.mentions.users.first();
 
@@ -34,8 +21,6 @@ if(!mention) return message.channel.send(`${this.client.emojis.find("name", "wro
 con.query(`DELETE FROM inventory WHERE id = ${mention.id}`)
 		 
 message.channel.send(`**${mention.tag}** a été reset !`)
-
-setTimeout(() => {con.end()}, 1000*5) 
 
 } 
 } 
