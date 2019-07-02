f8c62b4fbb2b404edab9ec8ced1bb1726d955ac0
pafad <32877302+pafad@users.noreply.github.com>
2019-07-02 15:40:49 +0200

    Update toprep.js

--- commands/Games/toprep.js
+++ commands/Games/toprep.js
@@ -14,18 +14,6 @@ aliases:[]
 
 run(message, args, level) {
 
-var con = mysql.createConnection({
-host:process.env.host, 
-user:process.env.user, 
-password:process.env.password, 
-database:process.env.database 
-}) 
-
-con.connect(err => {
-if(err) throw err;
-console.log("Base de données connecté.") 
-}) 
-
 con.query("SELECT * FROM inventory ORDER BY cast (rep as SIGNED) DESC LIMIT 25", (err, rows) => {
  
 let resp = ``;
@@ -39,8 +27,6 @@ resp += `[${isNaN(parseInt(i)+1) ? "-" : parseInt(i)+1}] - **${this.client.users
 message.channel.send(resp) 	
 	
 }) 
-
-setTimeout(() => {con.end()}, 1000*5) 
 
 }
 }
