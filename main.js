79a9b0121d815a32500f305374cacfc4e9edbce9
pafad <32877302+pafad@users.noreply.github.com>
2019-07-06 16:55:02 +0200

    Update main.js

--- main.js
+++ main.js
@@ -43,14 +43,14 @@ class Xenova extends Client {
   this.con; // Recreate the connection, since
                                                   // the old one cannot be reused.
 
-  con.connect(function(err) {              // The server is either down
+  this.con.connect(function(err) {              // The server is either down
     if(err) {                                     // or restarting (takes a while sometimes).
       console.log('error when connecting to db:', err);
       setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
     }                                     // to avoid a hot loop, and to allow our node script to
   });                                     // process asynchronous requests in the meantime.
                                           // If you're also serving http, display a 503 error.
-  con.on('error', function(err) {
+  this.con.on('error', function(err) {
     console.log('db error', err);
     if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
       handleDisconnect();                         // lost due to either server restart, or a
