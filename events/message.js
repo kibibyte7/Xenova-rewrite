c110ed02f84df609e66a08aa74a9fa4dfaf4512a
pafad <32877302+pafad@users.noreply.github.com>
2019-07-06 16:46:00 +0200

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,53 +1,3 @@
-const mysql = require("mysql") 
-var db_config = {
-host:process.env.host, 
-user:process.env.user, 
-password:process.env.password, 
-database:process.env.database, 
-useUnicode:true
-} 
-
-var con;
-
-function handleDisconnect() {
-  con = mysql.createConnection(db_config); // Recreate the connection, since
-                                                  // the old one cannot be reused.
-
-  con.connect(function(err) {              // The server is either down
-    if(err) {                                     // or restarting (takes a while sometimes).
-      console.log('error when connecting to db:', err);
-      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-    }                                     // to avoid a hot loop, and to allow our node script to
-  });                                     // process asynchronous requests in the meantime.
-                                          // If you're also serving http, display a 503 error.
-  con.on('error', function(err) {
-    console.log('db error', err);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-      handleDisconnect();                         // lost due to either server restart, or a
-    } else {                                      // connnection idle timeout (the wait_timeout
-      throw err;                                  // server variable configures this)
-    }
-  });
-}
-
-handleDisconnect();
-
-function RegenMana(){
- 
- con.query("SELECT * FROM inventory", (err, rows) => {
- 	
- for(var i in rows) {
- 
- con.query(`UPDATE inventory SET mana = ${parseInt(rows[i].mana)+1} WHERE id = ${rows[i].id}`)
- 
- if(rows[0].mana > rows[0].maxmana) return;
- }
-setTimeout(RegenMana, 1000*60)
-}) 
-} 
-
-RegenMana();
-
 module.exports = class {
   constructor(client) {
     this.client = client;
@@ -81,7 +31,7 @@ if (message.content.indexOf(settings.prefix) !== 0) return;
       .trim()
       .split(/ +/g);
     const command = args.shift().toLowerCase();
-
+    const con = this.client.con;
     if (message.guild && !message.member)
       await message.guild.fetchMember(message.author);
 
