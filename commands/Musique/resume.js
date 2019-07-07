4daeaa50230ed22c43d2b6a6cf6f9801fb62d200
pafad <32877302+pafad@users.noreply.github.com>
2019-07-07 21:26:14 +0200

    Create resume.js

---
+++ commands/Musique/resume.js
@@ -0,0 +1,25 @@
+const Command = require("../../modules/Command.js");
+
+class Resume extends Command {
+  constructor(client) {
+    super(client, {
+      name: "resume",
+      description: "Remet en marche la musique en pause.",
+      usage: "resume"
+    });
+  }
+
+  run(message) {
+    const serverQueue = message.client.queue.get(message.guild.id);
+    if (serverQueue && !serverQueue.playing) {
+      serverQueue.playing = true;
+      serverQueue.connection.dispatcher.resume();
+      return message.channel.send("⏯ Je remets en marche la musique !");
+    }
+    return message.channel.send(
+      `${this.client.emojis.find("name", "wrongMark")} Il y a aucune musique dans la playlist."
+    );
+  }
+}
+
+module.exports = Resume;
