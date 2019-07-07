0958fcb23c1396574ab1a6d32f2ebf5958b9e841
pafad <32877302+pafad@users.noreply.github.com>
2019-07-07 21:39:18 +0200

    Create stop.js

---
+++ commands/Musique/stop.js
@@ -0,0 +1,26 @@
+const Command = require("../../modules/Command.js");
+
+class Stop extends Command {
+  constructor(client) {
+    super(client, {
+      name: "stop",
+      description: "Arrêter la musique.",
+      usage: "stop"
+    });
+  }
+
+  run(message) {
+    const { voiceChannel } = message.member;
+    if (!voiceChannel)
+      return message.channel.send(
+        "Tu dois être dans un salon vocal pour utiliser cette commande !"
+      );
+    const serverQueue = message.client.queue.get(message.guild.id);
+    if (!serverQueue)
+      return message.channel.send(`${this.client.emojis.find("name", "wrongMark")} Il n'y a aucune musique dans la playlist.`);
+    serverQueue.songs = [];
+    serverQueue.connection.dispatcher.end("La musique a été stoppée !");
+  }
+}
+
+module.exports = Stop;
