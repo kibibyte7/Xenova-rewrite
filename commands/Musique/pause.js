5ec43ba812a18a807c942c0f87b5552e9aed2ac6
pafad <32877302+pafad@users.noreply.github.com>
2019-07-07 21:16:20 +0200

    Create pause.js

---
+++ commands/Musique/pause.js
@@ -0,0 +1,26 @@
+const Command = require("../../modules/Command.js");
+
+class Pause extends Command {
+  constructor(client) {
+    super(client, {
+      name: "pause",
+      description: "Arrêter la musique.",
+      category:"Musique", 
+      usage: "pause"
+    });
+  }
+
+  run(message) {
+    const serverQueue = message.client.queue.get(message.guild.id);
+    if (serverQueue && serverQueue.playing) {
+      serverQueue.playing = false;
+      serverQueue.connection.dispatcher.pause();
+      return message.channel.send("⏸ La musique à été en pause !");
+    }
+    return message.channel.send(
+      `${this.client.emojis.find("name", "wrongMark")} Il y a aucune musique dans la playlist."
+    );
+  }
+}
+
+module.exports = Pause;
