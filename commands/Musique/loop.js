f54cbeff83f7f7964401d55711bd88979d0d11ff
pafad <32877302+pafad@users.noreply.github.com>
2019-07-08 02:57:01 +0200

    Update loop.js

--- commands/Musique/loop.js
+++ commands/Musique/loop.js
@@ -12,18 +12,19 @@ class Loop extends Command {
 
   run(message) {
     const serverQueue = message.client.queue.get(message.guild.id);
-    if (serverQueue && serverQueue.loop){
+    if (serverQueue && serverQueue.loop)
       if(serverQueue.loop  == false) {
       serverQueue.loop = true;
+      serverQueue.connection.dispatcher.pause();
       return message.channel.send("🔄 boucle activée !");
     } else {
     serverQueue.loop = false;
+    serverQueue.connection.dispatcher.pause();
     return message.channel.send("🔄 boucle désactivée !");
     } 
     return message.channel.send(
       `${this.client.emojis.find("name", "wrongMark")} Il y a aucune musique dans la playlist.`
     );
-    } 
   }
 }
 
