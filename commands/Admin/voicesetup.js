e4aab6c2d1bcabc717aa2063efc814ff6548fb3e
pafad <32877302+pafad@users.noreply.github.com>
2019-08-15 16:07:30 +0200

    Create voicesetup.js

---
+++ commands/Admin/voicesetup.js
@@ -0,0 +1,32 @@
+const Command = require("../../modules/Command.js");
+
+class VoiceSetup extends Command {
+  constructor(client) {
+    super(client, {
+      name: "voiceseteup",
+      description: "Le bot fait une catégorie vocal privée.",
+      category:"Admin", 
+      usage: "voicesetup", 
+      aliases:["vs"] 
+    });
+  }
+
+  run(message, args) {
+  
+  message.channel.send(`${this.client.emojis.find(e => e.name === "typing")} Création de la catégorie...`).then(x => x.delete(2500))
+   
+  message.guild.createChannel("✨Vocal privé✨", "category").then(c => {
+  
+  message.guild.createChannel("➕ Crée ton salon", "voice").then(chan => {chan.setParent(c.id)})
+  
+  }).then(x => {
+  	
+  message.channel.send(`${this.client.emojis.find(e => e.name === "checkMark")} Voilà, la catégorie est faite !`) 
+  
+  }) 
+
+}	
+
+}
+
+module.exports = VoiceSetup;
