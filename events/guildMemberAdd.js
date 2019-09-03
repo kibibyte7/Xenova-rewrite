a17499b86454b9c575160021cc49938dc4ca6e14
pafad <32877302+pafad@users.noreply.github.com>
2019-09-03 14:06:01 +0200

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,3 +1,35 @@
+const mysql = require("mysql") 
+   var db_config = {
+    host:process.env.host, 
+    user:process.env.user, 
+    password:process.env.password, 
+    database:process.env.database, 
+    useUnicode:true
+    } 
+
+    function handleDisconnect() {
+    con = mysql.createConnection(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  con.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  this.con.on('error', function(err) {
+    //console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
+
 module.exports = class {
   constructor (client) {
     this.client = client;
@@ -6,5 +38,14 @@ module.exports = class {
   async run (member) {
 
     this.client.user.setActivity(`${this.client.config.defaultSettings.prefix}help | ${this.client.guilds.size} servs | ${this.client.users.size} utilisateurs`, {type:"STREAMING"});
+
+    con.query("SELECT * FROM settings WHERE guild_id = ${memeber.guild.id}, (err, rows) => {
+    
+    if(rows.length == 0) return;
+    
+    this.client.channel.get(rows.welcome_id).send(welcome_msg)
+
+    }) 
+
   }
 };
