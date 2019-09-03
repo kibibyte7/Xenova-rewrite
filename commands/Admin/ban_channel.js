93e2668a76a2e263d6cd7c7a13df0b0f7736de93
pafad <32877302+pafad@users.noreply.github.com>
2019-09-03 14:44:44 +0200

    Create ban_channel.js

---
+++ commands/Admin/ban_channel.js
@@ -0,0 +1,65 @@
+var sm = require('string-similarity')
+
+const Command = require("../../modules/Command.js");
+
+class Welcome_channel extends Command {
+  constructor(client) {
+    super(client, {
+      name: "welcome_channel",
+      description: "Définir un channel de bienvenue.",
+      category:"Admin", 
+      usage: "welcome_channel <channel>", 
+      permLevel:"XenoAdminPerm", 
+      aliases:["wc"] 
+    });
+  }
+
+  run(message, args, level, con) {
+  
+  if(args.length == 0) return message.channel.send(`${this.client.emojis.find("name", "wrongMark")} Entre un texte à tweeter.`) 
+  
+  let chans = [];
+
+      let indexes = [];
+
+      message.guild.channels.forEach(function(chan){
+
+      chans.push(chan.name)
+
+      indexes.push(chan.id)
+
+      })
+
+      let match = sm.findBestMatch(args.join(" "), chans);
+
+      let chan_name = match.bestMatch.target;
+
+      let Ctarget = message.guild.channels.get(indexes[chans.indexOf(chan_name)])
+      
+      let cible = message.mentions.channels.first() || message.guild.channels.find("id", args[0]) || Ctarget;
+      
+      if(!cible) return message.channel.send(`Je ne trouve pas ce channel.`) 
+      	
+      	con.query(`SELECT * FROM settings WHERE guild_id = ${message.guild.id}`, (err, rows) => {
+      	
+      	if(rows.length == 0){
+      		
+      	con.query(`INSERT INTO settings (welcome_id, leave_id, ban_id, welcome_msg, leave_msg, ban_msg, guild_id) VALUES (${cible.id}, ${cible.id}, ${cible.id}, 'non défini', 'non défini', 'non défini', ${message.guild.id})`)
+      
+      message.channel.send("Paramètres par défaut créé dans le channel: " + cible)
+            
+      return;
+      	
+      	}
+      	
+      	con.query(`UPDATE settings SET ban_id = ${cible} WHERE guild_id = ${message.guild.id}`) 
+      	
+      	message.channel.send("Le channel des bans est désormais : " + cible)
+            
+      	}) 
+      	
+}	
+
+}
+
+module.exports = Ban_channel;
