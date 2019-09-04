47ce93e01fe075c2d67a6e379dc42bbf77617e6d
pafad <32877302+pafad@users.noreply.github.com>
2019-09-04 11:59:56 +0200

    Update welcome_msg.js

--- commands/Admin/welcome_msg.js
+++ commands/Admin/welcome_msg.js
@@ -16,7 +16,7 @@ class Welcome_msg extends Command {
 
   run(message, args, level, con) {
   
-      if(args.length == 0) return message.channel.send(`${this.client.emojis.find("name", "wrongMark")} Entre un message de bienvenue\n**NOTE:** Voici quelques paramètres pour le message de bienvenue:\n\`{server}\` pour le nom du serveur.\n\`{user}\` pour le nouvel utilisateur.\n\`{membercount}\` pour le nombte de personnes sur oe serveur.`) 
+      if(args.length == 0) return message.channel.send(`${this.client.emojis.find("name", "wrongMark")} Entre un message de bienvenue\n**NOTE:** Voici quelques paramètres pour le message de bienvenue:\n\`{server}\` pour le nom du serveur.\n\`{user}\` pour le nouvel utilisateur.\n\`{membercount}\` pour le nombre de personnes sur le serveur.`) 
     
       	con.query(`SELECT * FROM settings WHERE guild_id = ${message.guild.id}`, (err, rows) => {
       	
@@ -30,7 +30,7 @@ class Welcome_msg extends Command {
       	
       	con.query(`UPDATE settings SET welcome_msg  = "${args.join(" ")}" WHERE guild_id = ${message.guild.id}`) 
       	
-      	message.channel.send("Le channel de bienvenue est désormais : **" + args.join(" ") +"**")
+      	message.channel.send("Le message de bienvenue est désormais : **" + args.join(" ") +"**")
             
       	}) 
       	
