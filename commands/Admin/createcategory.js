4463764eb8287917b5c689501aeb93707c97b268
pafad <32877302+pafad@users.noreply.github.com>
2019-09-05 23:09:41 +0200

    Update createcategory.js

--- commands/Admin/createcategory.js
+++ commands/Admin/createcategory.js
@@ -13,19 +13,15 @@ aliases:["addcat"]
 } 
 
 run(message, args, level) {
-if(!args || args.length < 1){
-         message.channel.send(`${this.client.emojis.find("name", "wrongMark")} Spécifie le nom de la catégorie à créer.`)
-         return;
-    }else{
+if(!args || args.length < 1) return message.channel.send(`${this.client.emojis.find("name", "wrongMark")} Spécifie le nom de la catégorie à créer.`); 
+         
       if(!message.guild.member(this.client.user).hasPermission("MANAGE_CHANNELS")) {
       message.channel.send(`${this.client.emojis.find("name", "wrongMark")} Je n'ai pas la permission de gérer les salons.`) 
       return;
       } else {
-       message.guild.createChannel(args.join(" "), "category")
-       message.channel.send(`${this.client.emojis.find("name", "checkMark")} J'ai créé la catégorie: **${args.join(" ")}**.`)
-        
-      }
-} 
+      message.guild.createChannel(args.join(" "), "category")
+      message.channel.send(`${this.client.emojis.find("name", "checkMark")} J'ai créé la catégorie: **${args.join(" ")}**.`)        
+      } 
 
 } 
 }
