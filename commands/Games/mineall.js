ef18a9393f76936e814c8148e2f16ce2cd7d862e
pafad <32877302+pafad@users.noreply.github.com>
2019-09-18 17:26:17 +0200

    Update mineall.js

--- commands/Games/mineall.js
+++ commands/Games/mineall.js
@@ -170,9 +170,11 @@ setTimeout(() => {
                 
    }
 
-	 con.query(`UPDATE inventory SET xp = ${pxp+randxp}, mana = 0, wood = ${pwood+randwood}, stone = ${pstone+randstone}, fer = ${pfer+randfer}, gold = ${por+randgold}, diament = ${pdiam+randdiam}, emeraude = ${pem+randem}, prismes = ${ppp+randpp}, antimatter = ${pam+randam}, osrizk = ${pos+randos} WHERE id = ${r[0].id}`)	    
+	 con.query(`UPDATE inventory SET xp = ${pxp+randxp}, totalxp = ${pxp+randxp}, mana = 0, wood = ${pwood+randwood}, stone = ${pstone+randstone}, fer = ${pfer+randfer}, gold = ${por+randgold}, diament = ${pdiam+randdiam}, emeraude = ${pem+randem}, prismes = ${ppp+randpp}, antimatter = ${pam+randam}, osrizk = ${pos+randos} WHERE id = ${r[0].id}`)	    
 	 message.channel.send(`${message.author} Tu as miné\n- ${randwood} Wood\n- ${randstone} Stone\n- ${randfer} Fer\n- ${randgold} Gold\n- ${randdiam} Diamants\n- ${randem} Émeraudes\n- ${randpp} Prismes-parfait\n- ${randam} Anti-matières\n- ${randos} Osrizk\n\nTu as gagné ${randxp} Xp\nMana utilisé : ${usedmana}`) 
-         if(r[0].xp > nxtLvl) con.query(`UPDATE inventory SET niveau = ${parseInt(r[0].niveau)+1} WHERE id = ${message.author.id}`)
+         if(r[0].xp > nxtLvl) con.query(`UPDATE inventory SET niveau = ${parseInt(r[0].niveau)+1}, xp = 0 WHERE id = ${message.author.id}`)
+         
+
 }, 500)
   
   }) 
