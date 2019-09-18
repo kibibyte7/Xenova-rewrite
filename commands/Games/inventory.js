47f146bbf268ca58305d23c419de304078717e3e
pafad <32877302+pafad@users.noreply.github.com>
2019-09-18 16:01:39 +0200

    Update inventory.js

--- commands/Games/inventory.js
+++ commands/Games/inventory.js
@@ -23,9 +23,9 @@ con.query(`SELECT * FROM inventory WHERE id = ${message.author.id}`, (err, rows)
  
  let guilde = 0;
 	
-	if(!rows.length == 1){
+	if(rows.length == 0){
 	try{
-	SQL = `INSERT INTO inventory(id, xp, totalxp, mana, maxmana, pui, category, guildename, guildowner, guildlevel, guildmember, guildmaxmembers, guildxp, guildtotalxp, pickaxe, wood, stone, fer, gold, diament, emeraude, prismes, antimatter, osrizk, attack, defense, niveau, tresors, rep, weaponlevel, pv, armorlevel, kills, atk_fragments, def_fragments, pick_fragments, armor_fragments, atk_enchant, def_enchant, pick_enchant, armor_enchant) VALUES (${message.author.id}, 0, 0, 25, 25, "null", "null", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)`
+	SQL = `INSERT INTO inventory(id, xp, totalxp, mana, maxmana, pui, category, guildename, guildowner, guildlevel, guildmember, guildmaxmembers, guildxp, guildtotalxp, pickaxe, wood, stone, fer, gold, diament, emeraude, prismes, antimatter, osrizk, attack, defense, niveau, tresors, rep, weaponlevel, pv, armorlevel, kills, atk_fragments, def_fragments, pick_fragments, armor_fragments, atk_enchant, def_enchant, pick_enchant, armor_enchant) VALUES (${message.author.id}, 0, 0, 25, 25, "null", "null", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)`
 
 	con.query(SQL)
 	
