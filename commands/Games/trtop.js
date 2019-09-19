8adc31d7478ea80adfd52d2861d8db66bc686243
pafad <32877302+pafad@users.noreply.github.com>
2019-09-19 13:58:08 +0200

    Update trtop.js

--- commands/Games/trtop.js
+++ commands/Games/trtop.js
@@ -13,21 +13,31 @@ aliases:[]
 
 run(message, args, level, con) {
 
-con.query("SELECT * FROM inventory ORDER BY cast (tresors as SIGNED) DESC LIMIT 25", (err, rows) => {
+con.query("SELECT * FROM inventory ORDER BY cast (tresors as SIGNED) DESC LIMIT 50", (err, rows) => {
  
 let resp = ``;
  		
 for(var i in rows){
-	
-resp += `[${isNaN(parseInt(i)+1) ? "-" : parseInt(i)+1}] - **${this.client.users.find("id", `${rows[i].id}`) == null ? "NULL" : this.client.users.find("id", `${rows[i].id}`).tag} - **Trésors: **${this.client.users.find("id", `${rows[i].id}`) == null ? "/": rows[i].tresors}**\n`
-		
+
+let u = this.client.users.find("id", `${rows[i].id}`) 
+			
+isNaN(i) ? `` : resp += `[${parseInt(i)+1}] - **${!u ? "invalid-user" : u.username}** - **Trésors: ${rows[i].tresors}**\n`
+
 } 
 	
-message.channel.send(resp) 	
+message.channel.send({embed:{
+color:0x010101,
+title:"Classement par Trésors récupérés", 
+description:`${resp}`, 
+timestamp:new Date(), 
+footer:{
+icon_url:this.client.user.avatarURL,
+text:`©️ Trtop | Xenova `
+} 
+}}) 
 	
 }) 
 
-}
 }
 
 module.exports = Trtop;
