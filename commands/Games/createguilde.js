748fe3411ddc766dda11dca41507aa8702eeb1b7
pafad <32877302+pafad@users.noreply.github.com>
2019-09-19 14:33:07 +0200

    Update createguilde.js

--- commands/Games/createguilde.js
+++ commands/Games/createguilde.js
@@ -24,12 +24,8 @@ con.query(`SELECT * FROM inventory WHERE id = ${message.author.id}`, (err, rows)
 	  if(rows[0].gold < 10000) return message.channel.send(`:x: Tu n'as pas les ressources nécessaires pour créer une guild, il te faut 10000 de gold.`); 
 	  
 	  if(parseInt(rows[0].guilde) !== 0) return message.channel.send(":x: Tu as déjà une guilde.") 
-	  
-	  con.query(`CREATE TABLE ${"guilde"+message.author.id}(name varchar(30), victory INT, defeat INT, guildlevel INT, guildxp INT, maxmember INT, open VARCHAR(30), members INT, ownerid varchar(30))`, (err, row) => {
-	  
-	  con.query(`INSERT INTO ${"guilde"+message.author.id}(name, victory, defeat, guildlevel, guildxp, maxmember, open, members, ownerid)  VALUES ('${args.join(" ").substring(0, 30)}', 0, 0, 1, 0, 25, 'true', 1, ${message.author.id})`);
-	  
-	  con.query(`UPDATE inventory SET gold = ${rows[0].gold-10000}, guilde = '${args.join(" ").substring(0, 30)}', guildowner = ${message.author.id} WHERE id = ${message.author.id}`);
+
+	  con.query(`UPDATE inventory SET gold = ${rows[0].gold-10000}, guildname = "${args.join(" ").substring(0, 30)}", guildowner = ${message.author.id}, guildlevel = 1, guildxp = 0, guildtotalxp = 0, guildvictory = 0, guilddefeat = 0, guildgrade = "Propriétaire", guildmembers = 1, guildmaxmembers = 10  WHERE id = ${message.author.id}`);
 	  
 	  message.channel.send(`La guilde : **${args.join(" ").substring(0, 30)}**a été créé avec succès.`) 
 	  
