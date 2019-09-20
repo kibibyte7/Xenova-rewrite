e5324fc8a0bb54f821fdb47e6050eba3e0e5c5f3
pafad <32877302+pafad@users.noreply.github.com>
2019-09-21 00:46:53 +0200

    Update tresor.js

--- commands/Games/tresor.js
+++ commands/Games/tresor.js
@@ -29,7 +29,7 @@ con.query("SELECT * FROM tresor", (err, rows) => {
    var randtime = Date.now()+Math.floor(Math.random()*1000*60*40) 
 	  	 
    if((rows[0].temps > Date.now()) && (rows[0].TEMPS !== 0)){         
-      message.channel.send(`Le trésor n'est pas disponible, il le sera dans **${minutes == 0 ? "" : minutes + " minutes"} ${seconds == 0 ? "" : seconds + " secondes"}**, le dernier trésor a été pris par ${rows[0].taker == "Xenova"? "Xenova" : this.client.users.find("id", `${rows[0].taker}`).tag} depuis le serveur ${rows[0].server == "Xenova" ? "Xenova Support" : rows[0].server}\nNOTE: Les messages envoyés par l'utilisateur et le bot sont supprimés pour réduire au maximum leurs nombres.`).then(m => {
+      message.channel.send(`Le trésor n'est pas disponible, il le sera dans **${minutes == 0 ? "" : minutes + " minutes"} ${seconds == 0 ? "" : seconds + " secondes"}**, tu l'as récupéré **${player[0].tresors}x**, le dernier trésor a été pris par ${rows[0].taker == "Xenova"? "Xenova" : this.client.users.find("id", `${rows[0].taker}`).tag} depuis le serveur ${rows[0].server == "Xenova" ? "Xenova Support" : rows[0].server}\nNOTE: Les messages envoyés par l'utilisateur et le bot sont supprimés pour réduire au maximum leurs nombres.`).then(m => {
       	m.delete(5000)
       	message.delete(5050)
       	}) 
@@ -44,13 +44,19 @@ con.query("SELECT * FROM tresor", (err, rows) => {
 	
           var randxp = Math.floor(Math.random()*100)*(player[0].tresors+1)
 	   
-	  con.query(`UPDATE inventory SET xp = ${parseInt(player[0].xp)+randxp}, totalxp = ${parseInt(player[0].totalxp)+randxp}, tresors = ${parseInt(player[0].tresors)+1} WHERE id = ${message.author.id}`) 	  
+          var randpp = Math.floor(Math.random()*100)*(player[0].tresors+1)
+	  
+          var randam = Math.floor(Math.random()*100)*(player[0].tresors+1)
+	  
+          var randgold = Math.floor(Math.random()*100)*(player[0].tresors+1)
+	    
+	  con.query(`UPDATE inventory SET xp = ${parseInt(player[0].xp)+randxp}, totalxp = ${parseInt(player[0].totalxp)+randxp}, gold = ${parseInt(player[0].gold)+randgold}, prismes = ${parseInt(player[0].prismes) + randpp}, prismes = ${parseInt(player[0].antimatter) + randam}, tresors = ${parseInt(player[0].tresors)+1} WHERE id = ${message.author.id}`) 	  
     	
     	  const nxtLvl = Math.floor(0.1 * Math.sqrt(player[0].xp));
     	
 	  if(player[0].niveau < nxtLvl) con.query(`UPDATE inventory SET niveau = ${parseInt(player[0].niveau)+1}, xp = 0, maxmana = ${parseInt(player[0].maxmana)+5}, attack = ${parseInt(player[0].attack)+3}, defense = ${parseInt(player[0].defense)+3}, pui = ${parseInt(player[0].pui) + 6} WHERE id = ${message.author.id}`)
 	  
-	  message.reply(`GG! Tu as eu le trésor, tu as gagné: **${randxp} xp**`) 
+	  message.reply(`GG! Tu as eu le trésor, tu as gagné: **${randgold} Gold**, **{randpp} Prismes-parfaits**, **${randam} Anti-matières** et **${randxp} xp** [COMBO: ${player[0].tresors}]`) 
 	  	
 	  }) 
 	  	
