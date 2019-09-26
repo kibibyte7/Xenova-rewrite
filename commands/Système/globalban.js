b2652ee66aeb8c1fcd1ebb7e2303c4c0c851d7e0
pafad <32877302+pafad@users.noreply.github.com>
2019-09-26 02:01:06 +0200

    Update globalban.js

--- commands/Système/globalban.js
+++ commands/Système/globalban.js
@@ -127,9 +127,7 @@ if(level !== 3) return message.channel.send(`${wrong} Tu n'es pas un développeu
 
 var mention = message.mentions.users.first() || this.client.users.find("id",args[1])
 
-var id = args[1].length == 18 && !isNaN(args[1])
-
-if(!mention || !id) return message.channel.send(`${wrong} Entre une id ou une mention d'utilisateur à bannir.`)
+if(!mention) return message.channel.send(`${wrong} Entre une id ou une mention d'utilisateur à bannir.`)
 
 con.query(`SELECT * FROM gban WHERE id = ${!mention ? args[1] : mention.id}`, (err, rows) => {
 
