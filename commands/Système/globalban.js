d5b3d4a5c07496bb8cb9e99d2b873b38391f81e8
pafad <32877302+pafad@users.noreply.github.com>
2019-09-26 13:04:28 +0200

    Update globalban.js

--- commands/Système/globalban.js
+++ commands/Système/globalban.js
@@ -131,7 +131,7 @@ if(!mention) return message.channel.send(`${wrong} Entre une id ou une mention d
 
 con.query(`SELECT * FROM gban WHERE id = ${!mention ? args[1] : mention.id}`, (err, rows) => {
 
-if(!rows) return message.channel.send(`${wrong} Cet utilisateur est déjà blacklist.`) 
+if(rows) return message.channel.send(`${wrong} Cet utilisateur est déjà blacklist.`) 
 
 con.query(`INSERT INTO gban (id, reason, date) VALUES (${!mention ? args[1] : mention.id}, "${args.slice(2).join(" ")}", "${new Date()}")`) 
 
