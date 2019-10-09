9925b6814cc3e4b201df6235c9e7be0353cc7f8d
pafad <32877302+pafad@users.noreply.github.com>
2019-10-09 23:52:04 +0200

    Update inrole.js

--- commands/Information/inrole.js
+++ commands/Information/inrole.js
@@ -76,13 +76,13 @@ var toFind = message.guild.roles.find("name", args.join(" ")) || toMention;
         setTimeout(() =>{m.react(right)},1000)
         setTimeout(() =>{m.react(wrong)},2000)
 
-        const filter = (reaction, user) => reaction.name === left.name && user.id === message.author.id || reaction.name === right.name && user.id === message.author.id || reaction.name === wrong.name && user.id === message.author.id 
+        const filter = (reaction, user) => reaction.emoji.name === left.name && user.id === message.author.id || reaction.emoji.name === right.name && user.id === message.author.id || reaction.emoji.name === wrong.name && user.id === message.author.id 
         
         const collect = m.createReactionCollector(filter);
         
         collect.on("collect", r => {
         
-        if(r.name === left.name){
+        if(r.emoji.name === left.name){
 
         r.remove(message.author) 
 
@@ -90,9 +90,9 @@ var toFind = message.guild.roles.find("name", args.join(" ")) || toMention;
 
         page--;
         
-        start -= 50;
+        start = start - 50;
 
-        end -= 50;
+        end = end - 50;
 
         m.edit({embed:{
             color:Math.floor(Math.random() * 16777214) + 1,
@@ -110,7 +110,7 @@ var toFind = message.guild.roles.find("name", args.join(" ")) || toMention;
         
         } 
         
-        if(r.name === right.name){
+        if(r.emoji.name === right.name){
 
         r.remove(message.author) 
 
@@ -138,7 +138,7 @@ var toFind = message.guild.roles.find("name", args.join(" ")) || toMention;
         
         } 
         
-        if(r.name === wrong.name){
+        if(r.emoji.name === wrong.name){
 
         m.edit({embed:{
         color:0xff0c69, 
