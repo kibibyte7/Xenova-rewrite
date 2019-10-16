d039940e4dd58b28937bb1024c12b70f1ac102bd
pafad <32877302+pafad@users.noreply.github.com>
2019-10-16 23:52:57 +0200

    Update guildDelete.js

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -4,6 +4,13 @@ module.exports = class {
   }
 
   async run (guild) {
+    
+
+    con.query("SELECT * FROM settings", (err, rows) => {
+
+    con.query(`DELETE FROM settings WHERE guild_id = ${guild.id}`)
+
+    }) 
 
     this.client.user.setActivity(`${this.client.config.defaultSettings.prefix}help | ${this.client.guilds.size} servs | ${this.client.users.size} utilisateurs`, {type:"STREAMING"});
     this.client.channels.get("586596533701443594").send(`[-] Le bot a quitté une guilde : **${guild.name} (${guild.id})** avec **${guild.memberCount - 1} membres**`);
