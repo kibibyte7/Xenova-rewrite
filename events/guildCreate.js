331fda40df9456aafd05ffdf563b3efa5eb2b1db
pafad <32877302+pafad@users.noreply.github.com>
2019-10-16 23:20:45 +0200

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -4,6 +4,14 @@ module.exports = class {
   }
 
   async run (guild) {
+    
+    con.query("SELECT * FROM settings", (err, rows) => {
+
+    con.query(`INSERT INTO settings (welcome_id, leave_id, ban_id, welcome_msg, leave_msg, ban_msg, user_autorole, bot_autorole, lang, guild_id) VALUES ('Non défini', 'Non défini', 'Non défini', 'Bienvenue sur **{server}**, {user} amuses toi bien ! 🤗', 'Mince, **{user}** nous a quitté... Bonne continuation à toi **{user}**...', 'Et paf ! **{user}** s\' est pris un coup de banhammer ! Tu n'as pas été sage.', 'Non défini', 'Non défini', 'en', ${guild.id}`)
+
+    }) 
+    
+    guild.owner.user.send(`Merci de m'avoir ajouté à ton serveur, voici quelques notes avant de m'utiliser :\nLes rôles pour avoir des permissions qur le bot est \`XenoModPerm\`(pour les modos) et \`XenoAdminPerm\` (pour les admins)\n Je suis un bot créé par **${this.client.users.find(u => u.id === "524996881198219276").tag}**`)
 
     this.client.user.setActivity(`${this.client.config.defaultSettings.prefix}help | ${this.client.guilds.size} servs | ${this.client.users.size} utilisateurs`, {type:"STREAMING"});
     this.client.channels.get("586596533701443594").send(`[+] Le bot a rejoint une nouvelle guilde : **${guild.name} (${guild.id})** avec **${guild.memberCount - 1} membres**`);
