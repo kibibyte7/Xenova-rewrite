3bc24e8705e661c49749ed3f746cacc85fa539c7
pafad <32877302+pafad@users.noreply.github.com>
2019-10-22 02:14:36 +0200

    Create lang.js

---
+++ commands/Admin/lang.js
@@ -0,0 +1,51 @@
+const Command = require("../../modules/Command.js");
+
+class Lang extends Command {
+  constructor(client) {
+    super(client, {
+      name: "lang",
+      description: "Changer la langue du bot.",
+      category:"Admin", 
+      usage: "lang <fr/en>", 
+      permLevel:"XenoAdminPerm", 
+      aliases:[] 
+    });
+  }
+
+  run(message, args, level, con) {
+  
+  if(args.length == 0) return message.channel.send(`${this.client.emojis.find("name", "wrongMark")} Entre un texte à tweeter.`) 
+  
+  if(args[0] === "fr") {
+
+  con.query(`SELECT * FROM settings WHERE guild_id = ${message.guild.id}`, (err, rows) => {
+  
+  if(rows[0].lang === args[0]) return message.channel.send(`${this.client.emojis.find(emote => emote.name === "wrongMark")} Le bot est déjà en Français.") 
+  
+  con.query(`UPDATE settings SET lang = '${args[0]}' WHERE guild_id = ${message.guild.id}`) 
+
+  message.channel.send(`${this.client.emojis.find(emote => emote.name === "checkMark ")} Le bot est maintenant en français, sur ce serveur.`) 
+
+  }) 
+
+  } 
+
+  if(args[0] === "en") {
+
+  con.query(`SELECT * FROM settings WHERE guild_id = ${message.guild.id}`, (err, rows) => {
+  
+  if(rows[0].lang === args[0]) return message.channel.send(`${this.client.emojis.find(emote => emote.name === "wrongMark")} The bot is already in English.") 
+  
+  con.query(`UPDATE settings SET lang = '${args[0]}' WHERE guild_id = ${message.guild.id}`) 
+
+  message.channel.send(`${this.client.emojis.find(emote => emote.name === "checkMark ")} The bot is now English, in this guild.`) 
+
+  }) 
+
+  } 
+
+} 
+
+}
+
+module.exports = Lang;
