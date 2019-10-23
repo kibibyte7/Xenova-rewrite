ad3848949e93be1b7a5192b9006f78a81397f109
pafad <32877302+pafad@users.noreply.github.com>
2019-10-23 12:41:49 +0200

    Update bingo.js

--- commands/Fun/bingo.js
+++ commands/Fun/bingo.js
@@ -7,8 +7,11 @@ class Bingo extends Command {
 constructor(client) {
 super(client, {
 name:"bingo", 
-description:"Deviner le nombre entre 0 et le nombre souhaité. (100 par défaut)", 
-category:"Fun", 
+FRdescription:"Deviner le nombre entre 0 et le nombre souhaité. (100 par défaut)", 
+ENdescription:"Guess the number between 0 and the desired number. (100 by default)", 
+category:"Fun",
+FRusage:"bingo [nombre]", 
+ENusage:"bingo [nombre]", 
 permLevel:"XenoModPerm" 
 }) 
 } 
