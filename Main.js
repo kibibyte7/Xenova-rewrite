c334b19bf84a062728b4517b174c930ced652ded
pafad <32877302+pafad@users.noreply.github.com>
2019-10-29 16:57:40 +0100

    Update Main.js

--- Main.js
+++ Main.js
@@ -6,6 +6,38 @@ const readdir = promisify(require("fs").readdir);
 const Enmap = require("enmap");
 const klaw = require("klaw");
 const path = require("path");
+const mysql = require("mysql") 
+
+   var db_config = {
+    host:process.env.host, 
+    user:process.env.user, 
+    password:process.env.password, 
+    database:process.env.database, 
+    useUnicode:true
+    } 
+
+    function handleDisconnect() {
+    con = mysql.createConnection(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  con.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  this.con.on('error', function(err) {
+    //console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect()
 
 class Xenova extends Client {
   constructor(options) {
@@ -77,6 +109,7 @@ class Xenova extends Client {
       const props = new (require(`${commandPath}${path.sep}${commandName}`))(
         this
       );
+      con.query(`DELETE FROM cooldown WHERE cmd = ${commandName}`) 
       this.logger.log(`Chargement de la commande: ${props.help.name}`, "log");
       props.conf.location = commandPath;
       if (props.init) {
