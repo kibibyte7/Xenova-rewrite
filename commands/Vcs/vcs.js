e56d61dc7d7a274b8ecadf5b87d525429e5fdd65
pafad <32877302+pafad@users.noreply.github.com>
2019-10-29 14:26:52 +0100

    Create vcs.js

---
+++ commands/Vcs/vcs.js
@@ -0,0 +1,109 @@
+const Command = require("../../modules/Command.js") 
+
+const sm = require("string-similarity");
+
+class SetChannel extends Command {
+constructor (client) {
+super(client, {
+name:"setchannel",
+category:"Vcs", 
+FRdescription:"Définir un channel de discussion interserveurs.",
+FRusage:"setchannel <#channel>/id ou nom",
+cooldown:5,
+aliases:[] 
+})
+} 
+
+
+run(message, args, level, con) {
+
+if(!args) return message.channel.send(`${this.client.emojis.find(e => e.name === "wrongMark")} Entre un message à envoyer.`)
+
+if(args.join(" ").length > 700) return message.channel.send(`${this.client.emojis.find(e => e.name === "wrongMark")} La limite de caractères est de 700, tu ne peux peux pas aller au-delà de ces limites.`)
+
+if(level == 3){
+
+con.query(`SELECT * FROM vcs_user WHERE id = ${message.author.id}`, (err, rows) => {
+
+if(rows.length == 0) con.query(`INSERT INTO vcs_user(id, grade, banned, bannedtime, bannedto) VALUES (${message.author.id}, '${this.client.emojis.find(e => e.name === "Certifier")} Développeur', ${false}, "Non défini", "Non défini"))`)
+
+}) 
+
+} else {
+
+con.query(`SELECT * FROM vcs_user WHERE id = ${message.author.id}`, (err, rows) => {
+
+if(rows.length == 0) con.query(`INSERT INTO vcs_user(id, grade, banned, bannedtime, bannedto) VALUES (${message.author.id}, '${this.client.emojis.find(e => e.name === "User")} Utilisateur', ${false}, "Non défini", "Non défini"))`)
+
+}) 
+
+} 
+
+con.query(`SELECT * FROM vcs WHERE id = ${message.channel.id}`, (err, rows) => {
+
+if(rows.length == 0) return message.channel.send(`${this.client.emojis.find(e => e.name === "wrongMark")} Ce channel n'est pas un channel de discussion interserveur.`).then(m => {
+
+m.delete(4000)
+
+message.delete(5000)
+
+})
+
+}) 
+
+con.query(`SELECT * FROM vcs_user WHERE id = ${message.author.id}`, (err, rows) => {
+
+if(rows.length == 0) return;
+
+if(rows[0].banned === "true") return message.channel.send(`${this.client.emojis.find(e => e.name === "wrongMark")} Tu as été banni temporairement voire définitivement du vcs, regarde les messages de ban.`) 
+
+}) 
+
+
+con.query("SELECT * FROM vcs", (err, rows) => {
+
+con.query("SELECT * FROM vcs_user WHERE id = ${message.author.id}`, (err, me) => {
+
+for(var i in rows){
+
+if(isNaN(i)) return;
+
+message.channel.send({embed:{
+color:Math.floor(Math.random() * 16777214) + 1,
+thumbnail:{
+url:message.author.avatarURL
+}, 
+fields:[{
+name:"Utilisateur :", 
+value:message.author.tag
+}, 
+{
+name:"Grade :", 
+value:me[0].grade
+}, 
+{
+name:"Message :", 
+value:args.join(" ") 
+}, 
+{
+name:"Depuis le serveur :", 
+value:message.guild.name
+}], 
+timestamp:new Date(), 
+footer:{
+icon_url:this.client.user.avatarURL,
+text:"© Vcs | Xenova" 
+}
+}}) 
+
+} 
+
+}) 
+
+}) 
+
+
+} 
+} 
+
+module.exports = Vcs;
