d3c31fedc175b9787f2747734a11272fd7e12fb9
pafad <32877302+pafad@users.noreply.github.com>
2019-10-30 01:38:17 +0100

    Update vcs.js

--- commands/Vcs/vcs.js
+++ commands/Vcs/vcs.js
@@ -65,7 +65,7 @@ con.query("SELECT * FROM vcs", (err, rows) => {
 
 con.query(`SELECT * FROM vcs_user WHERE id = ${message.author.id}`, (err, me) => {
 
-rows.forEach(function(c){
+rows.forEach(c => {
 
 bot.channels.get(c.id).send({embed:{
 color:Math.floor(Math.random() * 16777214) + 1,
