7ae5102d198128cd639f1d1c9cd10e0568edd619
pafad <32877302+pafad@users.noreply.github.com>
2019-10-30 16:02:23 +0100

    Create vcs_tempban.js

---
+++ commands/Vcs/vcs_tempban.js
@@ -0,0 +1,73 @@
+const Command = require("../../modules/Command.js") 
+
+class Vcs_tempban extends Command {
+constructor (client) {
+super(client, {
+name:"vcs_tempban",
+category:"Vcs", 
+FRdescription:"Bannir temporairement quelqu'un de la discussion interserveur.",
+FRusage:"vcs_tempban <id> <days> <raison>",
+aliases:[] 
+})
+} 
+
+
+run(message, args, level, con) {
+
+var id = this.client.findUserById(args[0]);
+
+var days = 1000*60*60*24*parseInt(args[1]);
+
+var reason = args.slice(2).join(" ");
+
+con.query(`SELECT * FROM vcs_user WHERE id = ${message.author.id}`, (err, rows) => {
+
+if(rows.length == 0) return;
+
+if(level !== 3 || rows[0].grade !== `${this.client.findEmoteByName("Modo")} Modérateur`) return message.channel.send(`${this.client.findEmoteByName("wrongMark")} Tu n'es pas modérateur du vcs.`)
+
+if(!id) return message.channel.send(`${this.client.findEmoteByName("wrongMark")} Entre une id à ban.`)
+
+if(isNaN(args[1])) return message.channel.send(`${this.client.findEmoteByName("wrongMark")} Entre un nombre de jour.`)
+
+if(!reason) return message.channel.send(`${this.client.findEmoteByName("wrongMark")} Entre une raison.`)
+
+con.query(`SELECT * FROM vcs_user WHERE id = ${id}`, (err, rows) => {
+
+con.query(`UPDATE vcs_user SET banned = "true", bannedtime = ${new Date()}, bannedto = ${new Date().getTime()+days} WHERE id = ${id}`)
+
+message.channel.send(`**${id.username}** a été banni pour : **${reason}** pendant une période de: **${days/(1000*60*60*24)} jours**.`)
+
+}) 
+
+}) 
+
+con.query(`SELECT * FROM vcs`, (err, chan) => {
+	
+for(var i in chan){
+
+let c = this.client.findChannelById(chan[i].id)
+
+c.send({embed:{
+title:"Système :", 
+color:Math.floor(Math.random() * 16777214) + 1,
+thumbnail:{
+url:this.client.user.avatarURL	
+}, 
+description:`**${id.username}** a été banni pour : **${reason}** pendant une période de: **${days/(1000*60*60*24)} jours**.`,
+timestamp:new Date(), 
+footer:{
+icon_url:this.client.user.avatarURL,
+text:"© Système vcs | Xenova" 
+} 
+}}) 
+
+} 	
+	
+}) 
+
+
+} 
+}
+
+module.exports = Vcs_tempban;
