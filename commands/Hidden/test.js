ff37121e27c0e033887909a011ecd674289d8a7d
pafad <32877302+pafad@users.noreply.github.com>
2019-11-02 00:26:15 +0100

    Update test.js

--- commands/Hidden/test.js
+++ commands/Hidden/test.js
@@ -17,6 +17,14 @@ class Test extends Command {
 
 async run(message, client, args, level){
 
+const firstNumber = Math.floor(Math.random()*10);
+
+const secondNumber = Math.floor(Math.random()*10);
+
+const thirdNumber = `${Math.floor(Math.random()*1000)-1}`;
+
+const result = `${firstNumber + second Number}` + `${thirdNumber}`;
+
 const canvas = Canvas.createCanvas(700, 250); 
 	
 const ctx = canvas.getContext('2d'); 
@@ -24,6 +32,8 @@ const ctx = canvas.getContext('2d');
 const background = await Canvas.loadImage("Images/background.png");
 
 ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+ctx.fillText(`${firstNumber} + ${secondNumber} = ? + "${thirdNumber}"`, canvas.width / 2.5, canvas.height / 1.8);
 
 const attachment = new Discord.Attachment(canvas.toBuffer(), 'captcha.png'); 
 
