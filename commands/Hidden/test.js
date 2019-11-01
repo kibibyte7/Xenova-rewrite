e4b4a8564eabf79f737dce86e9715a5d6b1129dd
pafad <32877302+pafad@users.noreply.github.com>
2019-11-01 23:38:08 +0100

    Create test.js

---
+++ commands/Hidden/test.js
@@ -0,0 +1,33 @@
+const Canvas = require("canvas") 
+
+const Command = require("../../modules/Command.js");
+
+class Test extends Command {
+  constructor(client) {
+    super(client, {
+      name: "test",
+      FRdescription:"Que du test dedans.", 
+      category:"Hidden", 
+      permissions:[], 
+      aliases:[] 
+    });
+  }
+async run(message, client, args, level){
+
+const canvas = Canvas.createCanvas(700, 250); 
+	
+const ctx = canvas.getContext('2d'); 
+
+const background = await Canvas.loadImage('../../Images/download.jpg');
+
+ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+const attachment = new Discord.Attachment(canvas.toBuffer(), 'captcha.png'); 
+
+message.channel.send("", attachement) 
+
+} 
+
+}
+
+module.exports = Test;
