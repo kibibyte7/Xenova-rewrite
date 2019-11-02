6a5637ac5464571fccbcf75f6077ef7eb86822f1
pafad <32877302+pafad@users.noreply.github.com>
2019-11-02 01:14:37 +0100

    Update test.js

--- commands/Hidden/test.js
+++ commands/Hidden/test.js
@@ -35,6 +35,8 @@ ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
 
 ctx.font = this.client.applyText(canvas, `${firstNumber} + ${secondNumber} = ? + "${thirdNumber}"`)   
 
+ctx.fillStyle = "#0d1bde";
+
 ctx.fillText(`${firstNumber} + ${secondNumber} = ? + "${thirdNumber}"`, canvas.width / 4, canvas.height / 1.8);
 
 const attachment = new Discord.Attachment(canvas.toBuffer(), 'captcha.png'); 
