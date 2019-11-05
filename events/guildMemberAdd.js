fdac94de7fd55179e3b615415595cf8ff8e05dca
pafad <32877302+pafad@users.noreply.github.com>
2019-11-05 04:20:51 +0100

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -72,15 +72,15 @@ module.exports = class {
     
     ctx.font = '25px NFS';
     ctx.fillStyle = '#ffffff';
-    ctx.fillText('Bienvenue sur le serveur,', canvas.width / 2.9, canvas.height / 3.5);
+    ctx.fillText('Bienvenue sur le serveur,', canvas.width / 2.9, canvas.height / 3.7);
 
     ctx.font = this.client.applyText(canvas, `${member.user.displayName}`) && '60px NFS';
     ctx.fillStyle = '#ffffff';
-    ctx.fillText(member.displayName, canvas.width / 2.9, canvas.height / 1.8);
+    ctx.fillText(member.displayName, canvas.width / 2.9, canvas.height / 2);
     
     ctx.font = `Amuses toi bien, nous sommes ${member.guild.members.size} Membres !` && '25px NFS';
     ctx.fillStyle = '#ffffff';
-    ctx.fillText(member.displayName, canvas.width / 2.9, canvas.height / 1);
+    ctx.fillText(`Amuses toi bien, nous sommes ${member.guild.members.size} Membres !`, canvas.width / 2.9, canvas.height / 1.3);
 
     ctx.beginPath();
 
