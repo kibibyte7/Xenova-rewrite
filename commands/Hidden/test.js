e67fb06b8a72583851892f60c55015156f25194a
pafad <32877302+pafad@users.noreply.github.com>
2019-11-12 23:54:12 +0100

    Update test.js

--- commands/Hidden/test.js
+++ commands/Hidden/test.js
@@ -1,6 +1,7 @@
 const pokefusion = require("pokefusion-api")
 const Command = require("../../modules/Command.js")
 const puppeteer = require("puppeteer") 
+const path = require("path") 
 
 class Pokefusion extends Command {
 constructor(client){
@@ -15,7 +16,7 @@ aliases:[]
 
 async run(message, args, level, con) {
 
-await pokefusion.getRandomFusion("https://www.google.com", {args: ['--no-sandbox', '--disable-setuid-sandbox']}).then(res => {
+await pokefusion.getRandomFusion(`${process.cwd()}${path.sep}.apt${path.sep}usr${path.sep}bin${path.sep}google-chrome`, {args: ['--no-sandbox', '--disable-setuid-sandbox']}).then(res => {
 console.log(res)
 });
 
