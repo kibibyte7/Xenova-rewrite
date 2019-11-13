0fe6226c8e6baa5e67d06cc282ef695d31660196
pafad <32877302+pafad@users.noreply.github.com>
2019-11-13 14:42:46 +0100

    Update test.js

--- commands/Hidden/test.js
+++ commands/Hidden/test.js
@@ -1,5 +1,5 @@
 const lib = require('lib');
-const pokefusion = lib.dan-jordan.pokefusion['@2.2.0'];
+const pokefusion = lib.danjordan.pokefusion['@2.2.0'];
 
 const Command = require("../../modules/Command.js")
 
