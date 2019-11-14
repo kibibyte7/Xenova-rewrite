34f6490a18370599bd9df31fed00e10ea3caf5c8
pafad <32877302+pafad@users.noreply.github.com>
2019-11-14 11:18:46 +0100

    Update boss.js

--- commands/Games/boss.js
+++ commands/Games/boss.js
@@ -5,7 +5,7 @@ class Boss extends Command {
 constructor(client){
 super(client, {
 name:"boss", 
-description:"Combattre le boss spécifié.", 
+FRdescription:"Combattre le boss spécifié.", 
 category:"Game", 
 usage:"boss attack <level>", 
 aliases:[] 
@@ -16,7 +16,8 @@ run(message, args, level, con) {
 
 con.query(`SELECT * FROM inventory WHERE id =${message.author.id}`, (err, rows) => {
     	
-    	if(rows.length == 0) return message.channel.send(":x:") 
+    	if(rows.length == 0) return message.channel.send(":x:")
+
     	let playerpv = parseInt(rows[0].pv);
     
     	if(playerpv == 0) return message.channel.send(":x: Tu n'as plus de pv.") 
@@ -35,7 +36,7 @@ con.query(`SELECT * FROM inventory WHERE id =${message.author.id}`, (err, rows) 
     	
     	let level = rows[0].niveau;
     	
-    	const nxtLvl = 500 * (Math.pow(2, rows[0].xp) - 1)*level;
+    	const nxtLvl = 500 * (Math.pow(2, rows[0].xp) - 1);
     	
     	let resp = ``;
     	
@@ -43,19 +44,24 @@ con.query(`SELECT * FROM inventory WHERE id =${message.author.id}`, (err, rows) 
     	let bossattack = bosses.bosses[args[1]-1].attack;
     	let bossdef = bosses.bosses[args[1]-1].defense;
     	let bossxp = bosses.bosses[args[1]-1].xp;
+        var atk_rune = Math.floor(Math.random()*4)*parseInt(args[1]) //atk_fragments
+        var def_rune = Math.floor(Math.random()*4)*parseInt(args[1]) //def_fragments
+        var pick = Math.floor(Math.random()*4)*parseInt(args[1]) //pick_fragments
+        var armor = Math.floor(Math.random()*4)*parseInt(args[1]) //armor_fragments
     	while(playerpv > 0 && bosspv > 0){
-    bosspv > 0 ? bosspv = bosspv-(rows[0].attack - bossdef < 0 ? 1 : rows[0].attack - bossdef): bosspv;
-    playerpv > 0 ? playerpv = playerpv -(bossattack - rows[0].defense < 0 ? 1 : bossattack - rows[0].defense) : playerpv; 
-    resp+= bosspv == 0 ? `` :`**${bosses.bosses[args[1]-1].name}** inflige ${bossattack - rows[0].defense < 0 ? 1 : bossattack - rows[0].defense} de dégâts à **${message.author.tag}** il lui reste ${playerpv} pv\n` 
-    resp+= playerpv == 0 ? `` :`TOUR: ${parseInt(turn)+1} - ${message.author} inflige ${rows[0].attack - bossdef < 0 ? 1 : rows[0].attack - bossdef} de dégâts à **${bosses.bosses[args[1]-1].name}** il lui reste ${bosspv}/${bosses.bosses[args[1]-1].hp}pv\n`
-    turn++;
+        bosspv > 0 ? bosspv = bosspv-(rows[0].attack - bossdef < 0 ? 1 : rows[0].attack - bossdef): bosspv;
+        playerpv > 0 ? playerpv = playerpv -(bossattack - rows[0].defense < 0 ? 1 : bossattack - rows[0].defense) : playerpv; 
+        resp+= bosspv == 0 ? `` :`**${bosses.bosses[args[1]-1].name}** inflige ${bossattack - rows[0].defense < 0 ? 1 : bossattack - rows[0].defense} de dégâts à **${message.author.tag}** il lui reste ${playerpv} pv\n` 
+        resp+= playerpv == 0 ? `` :`TOUR: ${parseInt(turn)+1} - ${message.author} inflige ${rows[0].attack - bossdef < 0 ? 1 : rows[0].attack - bossdef} de dégâts à **${bosses.bosses[args[1]-1].name}** il lui reste ${bosspv}/${bosses.bosses[args[1]-1].hp}pv\n`
+        turn++;
     	}
-    	
-    	message.channel.send(`${resp}\n${bosspv <= 0 ? "**"+bosses.bosses[args[1]-1].name+"** est mort ! \n**" +message.author.tag+"** remporte la victoire !" :"**"+message.author.tag+"** est mort ! \n**" +bosses.bosses[args[1]-1].name+"** remporte la victoire !"}`)
+        
+    	message.channel.send(resp+"\n"bosspv <= 0 ? `**${bosses.bosses[args[1]-1].name}** est mort ! \n**${message.author.tag}** remporte la victoire ! Récompenses:\n\n-Xp: ${bossxp}\n-Fragments d'attaque: ${atk_rune}\n-Fragments de défense: ${def_rune}\n-Fragments de pioche: ${pick}\n-Fragments d'armure: ${armor}` : `${message.author.tag}** est mort ! \n**${bosses.bosses[args[1]-1].name}** remporte la victoire !`)
     	
     	con.query(`UPDATE inventory SET pv = ${playerpv < 0 ? 0 : playerpv} WHERE id = ${message.author.id}`)
   
-    	if(bosspv == 0) con.query(`UPDATE inventory SET kills = ${parseInt(rows[0].kills)+1}, xp = ${parseInt(rows[0].xp)+bossxp} WHERE id = ${message.author.id}`) 
+    	if(bosspv <= 0) con.query(`UPDATE inventory SET kills = ${parseInt(rows[0].kills)+1}, xp = ${parseInt(rows[0].xp)+bossxp}, atk_fragments = ${parseInt(rows[0].atk_fragments)+atk_rune}, def_fragments = ${parseInt(rows[0].def_fragments)+def_rune}, pick_fragments = ${parseInt(rows[0].pick_fragments)+pick}, armor_fragments = ${parseInt(rows[0].armor_fragments)+armor} WHERE id = ${message.author.id}`) 
+
     	if(rows[0].xp > nxtLvl) con.query(`UPDATE inventory SET niveau = ${parseInt(rows[0].niveau)+1}, attack = ${parseInt(rows[0].attack)*rows[0].niveau+3}, defense = ${parseInt(rows[0].defense)*rows[0].niveau+3} WHERE id = ${message.author.id}`)
 	  
     	})
