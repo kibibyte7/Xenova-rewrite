4d4bc79958aee7f98232b19a7e92b067187ac0b2
pafad <32877302+pafad@users.noreply.github.com>
2019-11-14 14:17:28 +0100

    Create pokefusion.js

---
+++ commands/Fun/pokefusion.js
@@ -0,0 +1,42 @@
+const lib = require('lib');
+const pokefusion = lib.Hademar.pokefusion['@0.0.1'];
+
+const Command = require("../../modules/Command.js")
+
+class Pokefusion extends Command {
+constructor(client){
+super(client, {
+name:"pokefusion", 
+FRdescription:"Donne une image de Pokémon fusionné à un autre.", 
+category:"Fun", 
+usage:"pokefusion", 
+aliases:[] 
+}) 
+} 
+
+async run(message, args, level, con) {
+
+message.channel.startTyping();
+
+let result = await pokefusion();
+
+message.channel.send({embed:{
+color:Math.floor(Math.random()* 16777214) + 1,
+title:`Fusion [${result.name}] (l'image ne s'affiche pas ? Clique ici)`,
+url:`${result.imageUrl}`, 
+image:{
+url:`${result.imageUrl}`
+},
+timestamp: new Date(), 
+footer:{
+icon_url:this.client.user.avatarURL,
+text:"© Pokefusion | Xenova | Propulsé par l'api pokemon.alexonsager" 
+} 
+}}).then(m => {
+message.channel.stopTyping()
+})
+
+} 
+}
+
+module.exports = Pokefusion;
