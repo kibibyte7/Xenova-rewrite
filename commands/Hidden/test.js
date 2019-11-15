193962eaca76ffbe563bba2adfc3d7802bce4a06
pafad <32877302+pafad@users.noreply.github.com>
2019-11-15 02:29:07 +0100

    Update test.js

--- commands/Hidden/test.js
+++ commands/Hidden/test.js
@@ -40,7 +40,7 @@ var collect = m.createReactionCollector(filtre);
 
 con.query(`SELECT * FROM aki WHERE id = ${message.author.id}`, (err, rows) => {
 
-if(!rows) con.query(`INSERT Into aki(step, id) VALUES(1, ${message.author.id}`);
+if(!rows) con.query(`INSERT Into aki(step, id) VALUES(1, ${message.author.id})`);
 
 function stepcount(){
 con.query(`UPDATE aki SET step = ${rows[0].step + 1} WHERE id = ${message.author.id}`)
