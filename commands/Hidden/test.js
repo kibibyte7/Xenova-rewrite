09c29cc73c593f855e3edecd0f07e65ff271e308
pafad <32877302+pafad@users.noreply.github.com>
2019-11-18 09:42:47 +0100

    Update test.js

--- commands/Hidden/test.js
+++ commands/Hidden/test.js
@@ -23,7 +23,59 @@ aliases:[]
 run(message, args, level, con) {
 
 clash.clanByTag(args[0])
-.then(response => console.log(response))
+.then(clan => {
+message.channel.send({embed:{
+color:0x010101,
+title:`Informations sur le clan: ${clan.name}`,
+fields:[{
+name:"Type:", 
+value:clan.type === 'inviteOnly' ? "Sur invitation" : "Ouvert"
+},
+{
+name:"Nombre de membres:", 
+value:clan.members
+},
+{
+name:"Trophées requis:", 
+value:clan.requiredTrophies
+},
+{
+name:"Fréquence guerres de clans:", 
+value:clan.warFrequency === "always" ? "Toujours" : clan.warFrequency
+},
+{
+name:"Description:", 
+value:clan.description
+},
+{
+name:"Localisation:",
+value:clan.location.name
+}, 
+{
+name:"Niveau du clan:" 
+value:clan.clanLevel
+}, 
+{
+name:"Points du clan:" 
+value:`${clan.clanPoints} - ${clan.clanVersusPoints}`
+},
+{
+name:"Guerres de clans gagnées d'affilié:" 
+value:clan.warWinStreak
+},
+{
+name:"Guerres de clans gagnées en total:", 
+value:clan.warWins
+}],
+thumbnail:{
+url:clan.badgeUrls.large
+},
+timestamp:new Date(), 
+footer:{
+
+} 
+}}) 
+})
 .catch(e => console.log(e))
 
 } 
