6371f7069756dba6937415de60877e6f18836ca6
pafad <32877302+pafad@users.noreply.github.com>
2019-11-26 06:25:02 +0100

    Update guildMemberRemove.js

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,4 +1,7 @@
-const mysql = require("mysql") 
+const mysql = require("mysql")
+const Canvas = require("canvas") 
+const Discord = require("discord.js")
+
    var db_config = {
     host:process.env.host, 
     user:process.env.user, 
@@ -38,10 +41,52 @@ module.exports = class {
   async run (member) {
 
     this.client.user.setActivity(`${this.client.config.defaultSettings.prefix}help | ${this.client.guilds.size} servs | ${this.client.users.size} utilisateurs`, {type:"STREAMING"});
-  con.query(`SELECT * FROM settings WHERE guild_id = ${member.guild.id}`, (err, rows) => {
+    con.query(`SELECT * FROM settings WHERE guild_id = ${member.guild.id}`, (err, rows) => {
     
     if(!rows) return;
     
+    const canvas = Canvas.createCanvas(900, 250);
+	
+    const ctx = canvas.getContext('2d');
+    
+    Canvas.registerFont('Font/NFS.ttf', {family:"NFS"});
+
+    const background = await Canvas.loadImage('Images/joinleave1.jpg');
+    
+    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+    ctx.strokeStyle = '#74037b';
+
+    ctx.strokeRect(0, 0, canvas.width, canvas.height);
+    
+    ctx.font = '25px NFS';
+    ctx.fillStyle = '#ffffff';
+    ctx.fillText('Au revoir,', canvas.width / 3, canvas.height / 3.7);
+
+    ctx.font = this.client.applyText(canvas, `${member.user.displayName}`) && '35px NFS';
+    ctx.fillStyle = '#ffffff';
+    ctx.fillText(member.displayName, canvas.width / 3, canvas.height / 1.8);
+    
+    ctx.font = `Tu vas nous manquer, nous sommes ${member.guild.members.size} membres !` && '17px NFS';
+    ctx.fillStyle = '#ffffff';
+    ctx.fillText(`Tu vas nous manquer, nous sommes ${member.guild.members.size} membres !`, canvas.width / 3, canvas.height / 1.2);
+
+    ctx.beginPath();
+
+    ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
+
+    ctx.closePath();
+
+    ctx.clip();
+
+    const avatar = await Canvas.loadImage(member.user.displayAvatarURL);
+	
+    ctx.drawImage(avatar, 25, 0, 200, canvas.height);
+
+    const attachment = new Discord.Attachment(canvas.toBuffer(), 'leave_image.png');
+
+    let channel = member.guild.channels.find("id", rows[0].leave_id)
+
     let leave = rows[0].leave_msg
     
     var usertag = leave.replace("{user}", member.user.tag)
@@ -50,11 +95,9 @@ module.exports = class {
     
     var l = server.replace("{membercount}", member.guild.members.size) 
     
-    let channel = this.client.channels.find("id", rows[0].leave_id)
-
     if(!channel) return;
 
-    channel.send(this.client.toValues(l, "{user}", member.user.tag))
+    channel.send(this.client.toValues(l, "{user}", member.user.tag), attachment)
 
     }) 
 
