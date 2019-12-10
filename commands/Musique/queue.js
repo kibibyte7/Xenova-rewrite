d4a0ed8bf62db8efe70212786cf0ad96d66cf344
pafad <32877302+pafad@users.noreply.github.com>
2019-12-10 20:09:29 +0100

    Update queue.js

--- commands/Musique/queue.js
+++ commands/Musique/queue.js
@@ -16,10 +16,10 @@ class Queue extends Command {
     const serverQueue = message.client.queue.get(message.guild.id);
     
     if (!serverQueue)
-      return message.channel.send("Il n'y a aucune musique en cours !");
+      return message.channel.send(`${this.client.findEmoteByName("wrongMark")} Il n'y a aucune musique en cours !`);
     let songs = serverQueue.songs;
     for(var i in songs){
-    resp += isNaN(i) ? `` : `[${parseInt(i)+1}] - **${songs[i].title}** - Demandé par: **${songs[i].requester}**\n`
+    resp += isNaN(i) ? `` : `[${parseInt(i)+1}] - **${songs[parseInt(i)+1].title}** - Demandé par: **${songs[parseInt(i)+1].requester}**\n`
     } 
     
 message.channel.send(`
