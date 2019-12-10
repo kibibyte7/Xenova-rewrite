ee4601d760d91144f827abc2241b86b85c5f9237
pafad <32877302+pafad@users.noreply.github.com>
2019-12-10 20:36:03 +0100

    Update queue.js

--- commands/Musique/queue.js
+++ commands/Musique/queue.js
@@ -20,11 +20,11 @@ class Queue extends Command {
       return message.channel.send(`${this.client.findEmoteByName("wrongMark")} Il n'y a aucune musique en cours !`);
     let songs = serverQueue.songs;
     for(var i in songs){
-    songs.length == 1 ? resp += `Ajoute d'autres musiques avec +play <url/titre>` : resp += `[${parseInt(i)+1}] - **${songs[parseInt(i)+1].title}** - Demandé par: **${songs[parseInt(i)+1].requester}**\n`
+    songs.length == 1 ? resp += `` : resp += `[${parseInt(i)+1}] - **${songs[parseInt(i)+1].title}** - Demandé par: **${songs[parseInt(i)+1].requester}**\n`
     } 
     
 message.channel.send(`
-**Musique actuelle:** ${serverQueue.songs[0].title} demandé par **${serverQueue.songs[0].requester}**\n\n${resp}`
+**Musique actuelle:** ${serverQueue.songs[0].title} demandé par **${serverQueue.songs[0].requester}**\n\n${resp}\n\nAjoute d'autres musiques avec +play <url/titre>`
 );
   }
 }
