8e039ad3c694e1986aa37a274e5bc70eb4e8d553
pafad <32877302+pafad@users.noreply.github.com>
2019-12-16 19:48:58 +0100

    Update Shards.js

--- Shards.js
+++ Shards.js
@@ -1,5 +1,5 @@
 const { ShardingManager } = require('discord.js');
 const manager = new ShardingManager('./Main.js', { token: process.env.token});
 
-manager.spawn();
+manager.spawn(2);
 manager.on('launch', shard => console.log(`[SHARD] Shard ${shard.id} lancé`));
