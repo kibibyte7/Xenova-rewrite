01818fb44f5d42442f53220d440b29e84c4b4fbf
pafad <32877302+pafad@users.noreply.github.com>
2019-12-17 17:37:58 +0100

    Update bingo.js

--- commands/Fun/bingo.js
+++ commands/Fun/bingo.js
@@ -10,8 +10,7 @@ name:"bingo",
 FRdescription:"Deviner le nombre entre 0 et le nombre souhaité. (100 par défaut)", 
 ENdescription:"Guess the number between 0 and the desired number. (100 by default)", 
 category:"Fun",
-FRusage:"bingo [nombre]", 
-ENusage:"bingo [nombre]",
+usage:"bingo [nombre]",
 permissions:["USE_EXTERNAL_EMOJIS", "ADD_REACTION"], 
 permLevel:"XenoModPerm" 
 }) 
