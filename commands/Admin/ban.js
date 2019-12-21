8218301ccb395f76f75eff62691e9d88e357b775
pafad <32877302+pafad@users.noreply.github.com>
2019-12-21 15:30:38 +0100

    Update ban.js

--- commands/Admin/ban.js
+++ commands/Admin/ban.js
@@ -50,6 +50,10 @@ var waiting_msg = this.client.toValues(waiting_author, "{user}", mention.user.us
 var dm = this.client.toValues(lang.ban.dm, "{server}", message.guild.name)
 
 var dm_msg = this.client.toValues(dm, "{reason}", !args[1] ? lang.ban.no_reason : args.slice(1).join(" ")) 
+
+const check = this.client.findEmoteByName("checkMark");
+
+const wrong = this.client.findEmoteByName("wrongMark");
 	   	
 message.channel.send(waiting_msg).then(m => {
 	   	 
