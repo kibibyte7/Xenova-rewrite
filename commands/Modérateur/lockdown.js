a1cdba0e03ea02218906554cd5acb5df5f7c1977
pafad <32877302+pafad@users.noreply.github.com>
2020-03-27 21:13:14 +0100

    Update lockdown.js

--- commands/Modérateur/lockdown.js
+++ commands/Modérateur/lockdown.js
@@ -6,8 +6,10 @@ super(client, {
 name :"lockdown",
 FRdescription :"Empêche les utilisateurs de parler temporairement dans le channel.",
 ENdescription:"Prevents users from talking temporarily in the channel.", 
-usage:"lockdown <nombre> <min/h>",
-category:"Modérateur", 
+usage:"lockdown 2s | 2min | 2h",
+category:"Modérateur",
+cooldown:3,
+permissions:["SEND_MESSAGES", "USE_EXTERNAL_EOMJIS", "MANAGE_CHANNELS"],
 permLevel:"XenoModPerm", 
 aliases:["lock"] 
 })
@@ -17,19 +19,26 @@ run(message, args, level) {
 
 var mention = message.mentions.members.first(); 
     
-    const check = this.client.emojis.find("name", "checkMark")
+    const check = this.client.emojis.cache.find(e => e.name === "checkMark")
 
-    const wrong = this.client.emojis.find("name", "wrongMark")
+    const wrong = this.client.emojis.cache.find(e => e.name === "wrongMark")
 
-    if(isNaN(args[0]) || !args[1]){
+    if(isNaN(args[0].substr(0, args[0].length-1))) return message.channel.send(`${wrong} Utilise la commande comme ceci : **${this.client.config.defaultSettings.prefix}${this.client.commands.get("mute").help.usage}**`);
       
-      message.channel.send(`${wrong} Utilise la commande comme ceci : **${this.client.config.defaultSettings.prefix}${this.client.commands.get("mute").help.usage}**`);
-      
-      return;
-      
-    }else{
-    	  if(args[1] == "s") {
-    	  	message.channel.send(`${this.client.emojis.find("name","typing")} Veux tu vraiment bloquer le channel **${message.channel.name}** pour une durée de **${parseInt(args[0])} ${parseInt(args[0]) == 1 ? "seconde" : "secondes"}** ?`).then(m => {
+    if(args[0].endsWith("s") || args[0].endsWith("m") || args[0].endsWith("h")) {
+
+    let unity;
+    let multiplicateur;
+    
+    if(args[0].endsWith("s")) unity = "secondes" 
+    else if(args[0].endsWith("m")) unity = "minutes"
+    else if (args[0].endsWith("h")) unity = "heures"
+
+    if(args[0].endsWith("s")) multiplicateur = 1000
+    else if(args[0].endsWith("m")) multiplicateur = 6000
+    else if (args[0].endsWith("h")) multiplicateur = 36000
+    
+    message.channel.send(`${this.client.emojis.cache.find(e => e.name === "typing")} Veux tu vraiment bloquer le channel **${message.channel.name}** pour une durée de **${parseInt(args[0].substr(0, args[0].length-1))} ${unity}** ?`).then(m => {
       
       m.react(check);
       	
@@ -50,136 +59,33 @@ var mention = message.mentions.members.first();
       "CONNECT":false, 
       })
       
-      m.edit(`${check} Salon bloqué pour une durée de : **${parseInt(args[0])} ${parseInt(args[0]) == 1 ? "seconde" : "secondes"}**`)
+      m.edit(`${check} Salon bloqué pour une durée de : **${parseInt(args[0].substr(0, args[0].length-1))} ${unity}`)
        
       setTimeout(() => {        
-      	message.channel.overwritePermissions(message.guild.id, {
-      "SEND_MESSAGES":null, 
-      "CONNECT":null, 
-      })   
-      message.channel.send(`${check} Salon débloqué.`) 
-      }, parseInt(args[0])*1000);
-      
-      m.clearReactions(); 
-      m.edit(`${check} **${message.channel.name}** a été bloqué pour une durée de : **${parseInt(args[0])} ${parseInt(args[0]) == 1 ? "seconde" : "secondes"}**`)        
-      collect.stop();
-      } else {
-      	m.edit(`${wrong} lockdown annulé. `) 
-      	m.clearReactions();
-      	collect.stop();
-      	} 
-      	
-      	}) 
-        
-        
-      	}) 
-      	
-    	  } 
-    	
-    	
-      if(args[1] == "min") {
-      message.channel.send(`${this.client.emojis.find("name","typing")} Veux tu vraiment bloquer le channel **${message.channel.name}** pour une durée de **${parseInt(args[0])} ${parseInt(args[0]) == 1 ? "minute" : "minutes"}** ?`).then(m => {
-      
-      m.react(check);
-      	
-      	setTimeout(() => {m.react(wrong)}, 1000)  
-      	
-      	const filter = (reaction, user) => reaction.emoji.name == check.name && user.id == message.author.id || reaction.emoji.name == wrong.name && user.id == message.author.id;
-	     
-	     var collect = m.createReactionCollector(filter)
-	     
-	     collect.on('collect', r => {
-	     	
-	     	if(r.emoji.name == check.name){
-	     		
-	     r.remove(message.author);
-        
       message.channel.overwritePermissions(message.guild.id, {
-      "SEND_MESSAGES":false, 
-      "CONNECT":false, 
-      }) 
-      
-      m.edit(`${check} Salon bloqué pour une durée de : **${parseInt(args[0])} ${parseInt(args[1]) == 1 ? "minute" : "minutes"}**`)
-       
-      setTimeout(() => {        
-      	message.channel.overwritePermissions(message.guild.id, {
       "SEND_MESSAGES":null, 
       "CONNECT":null, 
       })   
       message.channel.send(`${check} Salon débloqué.`) 
-      }, parseInt(args[0])*60000);
+    }, parseInt(args[0].substr(0, args[0].length-1))*multiplicateur);
       
       m.clearReactions(); 
-      m.edit(`${check} **${message.channel.name}** a été bloqué pour une durée de : **${parseInt(args[0])} ${parseInt(args[0]) == 1 ? "minute" : "minutes"}**`)        
+      m.edit(`${check} **${message.channel.name}** a été bloqué pour une durée de : **${parseInt(args[0].substr(0, args[0].length-1))} ${unity}**`)        
       collect.stop();
       } else {
-      	m.edit(`${wrong} lockdown annulé. `) 
+      	m.edit(`${wrong} lockdown annulé.`) 
       	m.clearReactions();
       	collect.stop();
       	} 
       	
       	}) 
         
-        
-      	}) 
-      	      
-      }
-
-        if(args[1] == "h") {
-        	
-        message.channel.send(`${this.client.emojis.find("name","typing")} Veux tu vraiment bloquer le channel **${message.channel.name}** pour une durée de **${parseInt(args[0])} ${parseInt(args[0]) == 1 ? "heure" : "heures"}** ?`).then(m => {
-      
-      m.react(check);
-      	
-      	setTimeout(() => {m.react(wrong)}, 1000)  
-      	
-      	const filter = (reaction, user) => reaction.emoji.name == check.name && user.id == message.author.id || reaction.emoji.name == wrong.name && user.id == message.author.id;
-	     
-	     var collect = m.createReactionCollector(filter)
-	     
-	     collect.on('collect', r => {
-	     	
-	     	if(r.emoji.name == check.name){
-	     		
-	     r.remove(message.author);
-        
-      message.channel.overwritePermissions(message.guild.id, {
-      "SEND_MESSAGES":false, 
-      "CONNECT":false, 
-      }) 
-      
-      m.edit(`${check} Salon bloqué pour une durée de : **${parseInt(args[0])} ${parseInt(args[1]) == 1 ? "heure" : "heures"}**`)
-       
-      setTimeout(() => {        
-      	message.channel.overwritePermissions(message.guild.id, {
-      "SEND_MESSAGES":null, 
-      "CONNECT":null, 
-      })   
-      message.channel.send(`${check} Salon débloqué.`) 
-      }, parseInt(args[0])*360000);
-      
-      m.clearReactions(); 
-      m.edit(`${check} **${message.channel.name}** a été bloqué pour une durée de : **${parseInt(args[0])} ${parseInt(args[0]) == 1 ? "heure" : "heures"}**`)        
-      collect.stop();
-      } else {
-      	m.edit(`${wrong} lockdown annulé. `) 
-      	m.clearReactions();
-      	collect.stop();
-      	} 
-      	
-      	}) 
-        
-        
       	}) 
       	
-      	} 
-        
-      	
+    	  } 	
 
 }
 
-}  
-
-} 
+}
 
 module.exports = Lockdown;
