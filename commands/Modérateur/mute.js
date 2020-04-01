f06993d8a19f44decbe3b5c7c4c998b3836c94d1
pafad <32877302+pafad@users.noreply.github.com>
2020-04-02 01:41:38 +0200

    Update mute.js

--- commands/Modérateur/mute.js
+++ commands/Modérateur/mute.js
@@ -6,7 +6,10 @@ super(client, {
 name :"mute",
 FRdescription :"Empêche un utilisateur de parler temporairement.",
 Endescription:"Prevents a user from speaking temporarily.", 
-usage:"mute <@mention> <nombre> <min/h>",
+usage:"mute <@mention> 1s | 18m | 24h",
+permissions:["MANAGE_ROLES", "MANAGE_CHANNELS", "MANAGES_MESSAGES", "USE_EXTERNAL_EMOJIS"],
+cooldown:3,
+guildOnly:true,
 category:"Modérateur", 
 permLevel:"XenoModPerm"
 })
@@ -14,21 +17,41 @@ permLevel:"XenoModPerm"
 
 run(message, args, level) {
 
-var mention = message.mentions.members.first(); 
+    var mention = message.mentions.members.first(); 
     
-    const check = this.client.emojis.find("name", "checkMark")
+    const check = this.client.emojis.cache.find(e => e.name === "checkMark")
 
-    const wrong = this.client.emojis.find("name", "wrongMark")
+    const wrong = this.client.emojis.cache.find(e => e.name === "wrongMark")
 
-    if(!mention || isNaN(args[1]) || !args[2]){
+    var endFilter = args[1].endsWith("s") || args[1].endsWith("m") || args[1].endsWith("h");
+
+    let unity;
+
+    let multiplicateur;
+
+    let role = message.guild.roles.caches.find(r => r.name === "XenoMute")
+    
+    if(!role){
+
+    var muteRole = message.guild.roles.create({data:{name:"XenoMute", color:0x010101, permissions:["VIEW_CHANNEL"]}})
+
+    message.guild.channels.cache.map(c => c.updateOverwrite(muteRole.id, { SEND_MESSAGES:false, STREAM:false, CONNECT:false, ADD_REACTION:false}))
+
+    return message.channel.send(`${this.client.emojis.cache.find(e => e.name === "checkMark")} J'ai créé le rôle \`${muteRole.name}\` pour toi car il était inexistant dans la guilde.`)
+    
+    }
+    
+    if(!mention || isNaN(args[1]) || !endFilter) return message.channel.send(`${wrong} Utilise la commande comme ceci : **${this.client.config.defaultSettings.prefix}${this.client.commands.get("mute").help.usage}**`);
       
-      message.channel.send(`${wrong} Utilise la commande comme ceci : **${this.client.config.defaultSettings.prefix}${this.client.commands.get("mute").help.usage}**`);
+      if(args[1].endsWith("s")) unity = "secondes"
+      else if(args[1].endsWith("m")) unity = "minutes"
+      else if(args[1].endsWith("h")) utity = "heures"
+
+      if(args[1].endsWith("s")) multiplicateur = 1000
+      else if(args[1].endsWith("m")) multiplicateur = 60000
+      else if(args[1].endsWith("h")) multiplicateur = 3600000
       
-      return;
-      
-    }else{
-      if(args[2] == "min") {
-      message.channel.send(`${this.client.emojis.find("name","typing")} Veux tu vraiment mute **${mention.user.username}** pour une durée de **${parseInt(args[1])} ${parseInt(args[1]) == 1 ? "minute" : "minutes"}** ?`).then(m => {
+      message.channel.send(`${this.client.emojis.cache.find(e => e.name ==="typing")} Veux tu vraiment mute **${mention.user.username}** pour une durée de **${parseInt(args[1].substr(0, args[1].length-1))} ${unity}** ?`).then(m => {
       
       m.react(check);
       	
@@ -40,83 +63,37 @@ var mention = message.mentions.members.first();
 	     
 	     collect.on('collect', r => {
 	     	
-	     	if(r.emoji.name == check.name){
+	     if(r.emoji.name == check.name){
 	     		
-	     r.remove(message.author);
-        
-      message.guild.channels.map(c => c.overwritePermissions(mention.user.id, {
-      "SEND_MESSAGES":false, 
-      "CONNECT":false, 
-      "ADD_REACTIONS":false, 
-      })) 
-        
-      setTimeout(() => {        
-      message.guild.channels.map(c => c.permissionOverwrites.get(mention.user.id).delete())        
-      }, parseInt(args[1])*60000);
-      
-      m.clearReactions(); 
-      m.edit(`${check} **${mention.user.username}** a été mute pour une durée de : **${parseInt(args[1])} ${parseInt(args[1]) == 1 ? "minute" : "minutes"}**`)        
-      collect.stop();
-      } else {
-      	m.edit(`${wrong} mute annulé. `) 
-      	m.clearReactions();
-      	collect.stop();
-      	} 
-      	
-      	}) 
-        
-        
-      	}) 
-      	}
-        }
+	     r.users.remove(message.author);
 
-        if(args[2] == "h") {
-        message.channel.send(`${this.client.emojis.find("name","typing")} Veux tu vraiment mute **${mention.user.username}** pour une durée de **${parseInt(args[1])} ${parseInt(args[1]) == 1 ? "heure" : "heures"}** ?`).then(m => {
+       mention.roles.add(role);
       
-      m.react(check);
-      	
-      	setTimeout(() => {m.react(wrong)}, 1000)  
-      	
-      	const filter = (reaction, user) => reaction.emoji.name == check.name && user.id == message.author.id || reaction.emoji.name == wrong.name && user.id == message.author.id;
-	     
-	     var collect = m.createReactionCollector(filter)
-	     
-	     collect.on('collect', r => {
-	     	
-	     	if(r.emoji.name == check.name){
-	     		
-	     r.remove(message.author);
-        
-      message.guild.channels.map(c => c.overwritePermissions(mention.user.id, {
-      "SEND_MESSAGES":false, 
-      "CONNECT":false, 
-      "ADD_REACTIONS":false, 
-      })) 
-        
       setTimeout(() => {        
-      message.guild.channels.map(c => c.permissionOverwrites.get(mention.user.id).delete())        
-      }, parseInt(args[1])*360000);
+
+      mention.roles.remove(role.id);
+       
+      }, parseInt(args[1].substr(0, args[1].length-1))*multiplicateur);
       
-      m.clearReactions(); 
-      m.edit(`${check} **${mention.user.username}** a été mute pour une durée de : **${parseInt(args[1])} ${parseInt(args[1]) == 1 ? "heure" : "heures"}**`)        
+      m.reactions.removeAll(); 
+      m.edit(`${check} **${mention.user.username}** a été mute pour une durée de : **${parseInt(args[1].substr(0, args[1].length-1))} ${unity}**`)        
       collect.stop();
       } else {
       	m.edit(`${wrong} mute annulé. `) 
-      	m.clearReactions();
+      	m.reactions.removeAll();
       	collect.stop();
       	} 
       	
       	}) 
         
         
-      	}) 
-      	} 
+      	})
         
       	
 
 
 }  
 
-} 
+}
 
 module.exports = Mute;
