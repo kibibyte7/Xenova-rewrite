ebd16cfcd124f1b2b3bcf3ad6f13d75fec8f4793
pafad <32877302+pafad@users.noreply.github.com>
2020-04-03 14:52:30 +0200

    Update volume.js

--- commands/Musique/volume.js
+++ commands/Musique/volume.js
@@ -13,18 +13,18 @@ class Volume extends Command {
     const { voiceChannel } = message.member;
     if (!voiceChannel)
       return message.channel.send(
-        `${message.client.emojis.find("name","wrongMark")} Tu être dans un salon vocal pour utiliser cette commande !`
+        `${message.client.emojis.cache.find(e => e.name === "wrongMark")} Tu être dans un salon vocal pour utiliser cette commande !`
       );
     const serverQueue = message.client.queue.get(message.guild.id);
     if (!serverQueue)
       return message.channel.send("Il n'y a aucune musique en cours !");
     if (isNaN(args[0])) 
       return message.channel.send(
-        `${message.client.emojis.find("name","checkMark")} Le volume actuel est: **${serverQueue.volume}**`
+        `${message.client.emojis.cache.find(e => e.name ==="checkMark")} Le volume actuel est: **${serverQueue.volume}**`
       );
     serverQueue.volume = args[0];
     serverQueue.connection.dispatcher.setVolumeLogarithmic(args[0] / 5);
-    return message.channel.send(`${message.client.emojis.find("name","checkMark")} J'ai mis le volume à: **${args[0]}**`);
+    return message.channel.send(`${message.client.emojiscache.find(e => e.name ==="checkMark")} J'ai mis le volume à: **${args[0]}**`);
   }
 }
 
