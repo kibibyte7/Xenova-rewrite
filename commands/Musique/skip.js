52c7b591596b5ec30be42a896bf5c320eae896c7
pafad <32877302+pafad@users.noreply.github.com>
2020-04-03 14:51:08 +0200

    Update skip.js

--- commands/Musique/skip.js
+++ commands/Musique/skip.js
@@ -10,13 +10,13 @@ class Skip extends Command {
   }
 
   run(message) {
-    const skip = message.client.emojis.find("name","Skip")
+    const skip = this.client.emojis.cache.find(e => e.name ===,"Skip")
     const { voiceChannel } = message.member;
     if (!voiceChannel)
-      return message.channel.send(`${this.client.emojis.find("name", "wrongMark")} Tu dois être dans un salon vocal pour utiliser cette commande !`);
+      return message.channel.send(`${this.client.emojis.cache.find(e => e.name === "wrongMark")} Tu dois être dans un salon vocal pour utiliser cette commande !`);
     const serverQueue = message.client.queue.get(message.guild.id);
     if (!serverQueue)
-      return message.channel.send(`${this.client.emojis.find("name", "wrongMark")} Il n'y a aucune musique dans la playlist.`);
+      return message.channel.send(`${this.client.emojis.cache.find(e => e.name === "wrongMark")} Il n'y a aucune musique dans la playlist.`);
     
     message.channel.send(`${skip} La musique : **${serverQueue.songs[0].title}** a été skip.`)
     serverQueue.connection.dispatcher.end("La musique a été skip.");
