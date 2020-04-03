67b622cf282bba25e33236b1df5d92414a3e22cf
pafad <32877302+pafad@users.noreply.github.com>
2020-04-03 14:54:52 +0200

    Update queue.js

--- commands/Musique/queue.js
+++ commands/Musique/queue.js
@@ -18,9 +18,9 @@ class Queue extends Command {
     
     if (!serverQueue)
       return message.channel.send(`${this.client.findEmoteByName("wrongMark")} Il n'y a aucune musique en cours !`);
-    let songs = serverQueue.songs;
+    let songs = serverQueue.songs.slice(1);
     for(var i in songs){
-    isNaN(i) ? resp += `` : resp += `[${parseInt(i)+1}] - **${songs[parseInt(i)+1].title}** - Demandé par: **${songs[parseInt(i)+1].requester}**\n`
+    !isNaN(i) ? resp += `[${parseInt(i)+1}] - **${songs[parseInt(i)].title}** - Demandé par: **${songs[parseInt(i)].requester}**\n` :_``;
     } 
     
 message.channel.send(`
