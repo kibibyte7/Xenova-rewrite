bd657874c09e2572fbd8b8eb9b5caea7b2619dae
pafad <32877302+pafad@users.noreply.github.com>
2020-04-04 18:06:15 +0200

    Update queue.js

--- commands/Musique/queue.js
+++ commands/Musique/queue.js
@@ -26,7 +26,20 @@ class Queue extends Command {
     !isNaN(i) ? resp += `[${parseInt(i)+1}] - **${songs[parseInt(i)].title}** - Demandé par: **${songs[parseInt(i)].requester}**\n` :``;
     } 
     
-     message.channel.send(`**Musique actuelle:** ${serverQueue.songs[0].title} demandé par **${serverQueue.songs[0].requester}**\n\n${resp}\n\nAjoute d'autres musiques avec +play <url/titre>`);
+    +eval var songs = message.client.queue.get(message.guild.id).songs;
+
+    var time = 0;
+
+   for(var i in songs){
+
+   isNaN(i) ? `` : time = parseInt(time)+parseInt(songs[i].duration);
+
+   }
+
+   var minutes = Math.floor(time/60);
+   var seconds = Math.floor(time%60);
+      
+   message.channel.send(`**Musique actuelle:** ${serverQueue.songs[0].title} demandé par **${serverQueue.songs[0].requester}**\n\nDurée totale: **${minutes} Minutes et ${seconds} Secondes**\n\n${resp}\n\nAjoute d'autres musiques avec +play <url/titre>`);
     
       
     } else {
