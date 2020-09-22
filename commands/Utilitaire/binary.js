029d673bb59255d19c432042b315147e7c8f0b2c
pafad <32877302+pafad@users.noreply.github.com>
2020-09-22 20:15:13 +0200

    Update binary.js

--- commands/Utilitaire/binary.js
+++ commands/Utilitaire/binary.js
@@ -14,6 +14,34 @@ class Binary extends Command {
 
     run(message, args, level) {
 
+        if (args[1] === "info") {
+
+            return message.channel.send({
+                embed: {
+                    color: 0x000444,
+                    fields: [
+                        {
+                            name: `Possibilités`,
+                            value: `${args[0] === "s" ? file.signed_binary.length : file.natural_binary.length}`
+                        },
+                        {
+                            name: `Minimum (${args[0] === "s" ? "Binaire signé" : "Binaire naturel"})`,
+                            value: `${args[0] === "s" ? file.signed_binary.find(d => d.decimal === "-128").decimal : file.natural_binary.find(d => d.decimal === "0").decimal}`
+                        },
+                        {
+                            name: `Maximum (${args[0] === "s" ? "Binaire signé" : "Binaire naturel"})`,
+                            value: `${args[0] === "s" ? file.signed_binary.find(d => d.decimal === "127").decimal + 1 : file.natural_binary.length - 1}`
+                        }
+                    ],
+                    timestamp: new Date(),
+                    footer: {
+                        text: `Binary info (${args[0] === "s" ? "signé" : "naturel"}) Xenova`,
+                        icon_url: message.author.avatarURL()
+                    }
+                }
+            })
+        }
+
         if (!args[0] || isNaN(args[1])) {
 
             this.client.getHelp(message, this.help.name, level);
@@ -25,14 +53,12 @@ class Binary extends Command {
         var natural = file.natural_binary.filter(b => b.decimal == args[1]).find(d => d.decimal == args[1]);
         var signed = file.signed_binary.filter(b => b.decimal == args[1]).find(d => d.decimal == args[1]);
 
-        console.log(natural);
-
         message.channel.send({
             embed: {
                 color: 0x010101,
                 fields: [{
                     name: "Décimale:",
-                    value: args[0] === "n" ?  natural.decimal : signed.decimal
+                    value: args[0] === "n" ? natural.decimal : signed.decimal
                 },
                 {
                     name: "Binaire:",
